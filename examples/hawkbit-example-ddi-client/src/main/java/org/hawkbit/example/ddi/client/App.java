abe6dc223f9d3c2f643142e274a9f3da0cfbf48b
Jonathan Philip Knoblauch <JonathanPhilip.Knoblauch@bosch-si.com>
2016-04-18 12:51:57 +0200

    Created hawkbit-example-ddi-client example maven module
    Signed-off-by: Jonathan Philip Knoblauch <JonathanPhilip.Knoblauch@bosch-si.com>

---
+++ examples/hawkbit-example-ddi-client/src/main/java/org/hawkbit/example/ddi/client/App.java
@@ -0,0 +1,13 @@
+package org.hawkbit.example.ddi.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
