3851a6c1dfae45511d407ec03efdcfea55da99cb
Jonathan Philip Knoblauch <JonathanPhilip.Knoblauch@bosch-si.com>
2016-04-27 16:47:14 +0200

    Fixed sonar issues and added javadoc
    Signed-off-by: Jonathan Philip Knoblauch <JonathanPhilip.Knoblauch@bosch-si.com>

--- examples/hawkbit-example-ddi-client/src/main/java/org/eclipse/hawkbit/ddi/client/DdiDefaultFeignClient.java
+++ examples/hawkbit-example-ddi-client/src/main/java/org/eclipse/hawkbit/ddi/client/DdiDefaultFeignClient.java
@@ -11,6 +11,7 @@ package org.eclipse.hawkbit.ddi.client;
 import org.apache.commons.lang.Validate;
 import org.eclipse.hawkbit.ddi.client.authenctication.AuthenticationInterceptor;
 import org.eclipse.hawkbit.ddi.client.resource.RootControllerResourceClient;
+import org.eclipse.hawkbit.ddi.client.resource.RootControllerResourceClientConstants;
 import org.eclipse.hawkbit.feign.core.client.ApplicationJsonRequestHeaderInterceptor;
 import org.eclipse.hawkbit.feign.core.client.IgnoreMultipleConsumersProducersSpringMvcContract;
 
@@ -26,26 +27,39 @@ import feign.jackson.JacksonEncoder;
 public class DdiDefaultFeignClient {
 
     private RootControllerResourceClient rootControllerResourceClient;
-
     private final Builder feignBuilder;
     private final String baseUrl;
     private final String tenant;
 
+    /**
+     * Constructor for default DDI feign client with no authentication.
+     * 
+     * @param baseUrl
+     *            the base url of the client
+     * @param tenant
+     *            the tenant
+     */
     public DdiDefaultFeignClient(final String baseUrl, final String tenant) {
         this(baseUrl, tenant, null);
     }
 
+    /**
+     * Constructor for default DDI feign client with authentication.
+     * 
+     * @param baseUrl
+     *            the base url of the client
+     * @param tenant
+     *            the tenant
+     * @param authenticationInterceptor
+     */
     public DdiDefaultFeignClient(final String baseUrl, final String tenant,
             final AuthenticationInterceptor authenticationInterceptor) {
         feignBuilder = Feign.builder().contract(new IgnoreMultipleConsumersProducersSpringMvcContract())
-                .requestInterceptor(new ApplicationJsonRequestHeaderInterceptor())
-                .requestInterceptor(new DdiAcceptedRequestInterceptor()).logLevel(Level.FULL)
+                .requestInterceptor(new ApplicationJsonRequestHeaderInterceptor()).logLevel(Level.FULL)
                 .logger(new Logger.ErrorLogger()).encoder(new JacksonEncoder()).decoder(new DdiDecoder());
-
         if (authenticationInterceptor != null) {
             feignBuilder.requestInterceptor(authenticationInterceptor);
         }
-
         Validate.notNull(baseUrl, "A baseUrl has to be set");
         Validate.notNull(tenant, "A tenant has to be set");
         this.baseUrl = baseUrl;
@@ -69,7 +83,7 @@ public class DdiDefaultFeignClient {
     public RootControllerResourceClient getRootControllerResourceClient() {
 
         if (rootControllerResourceClient == null) {
-            String rootControllerResourcePath = this.baseUrl + RootControllerResourceClient.PATH;
+            String rootControllerResourcePath = this.baseUrl + RootControllerResourceClientConstants.PATH;
             rootControllerResourcePath = rootControllerResourcePath.replace("{tenant}", tenant);
             rootControllerResourceClient = feignBuilder.target(RootControllerResourceClient.class,
                     rootControllerResourcePath);
---
+++ examples/hawkbit-example-ddi-client/src/main/java/org/eclipse/hawkbit/ddi/client/resource/RootControllerResourceClientConstants.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2011-2016 Bosch Software Innovations GmbH, Germany. All rights reserved.
+ */
+package org.eclipse.hawkbit.ddi.client.resource;
+
+/**
+ * 
+ * Constants for RootController resource client.
+ */
+public class RootControllerResourceClientConstants {
+
+    public static final String PATH = "{tenant}/controller/v1";
+
+    /**
+     * Default constructor.
+     */
+    private RootControllerResourceClientConstants() {
+
+    }
+
+}
