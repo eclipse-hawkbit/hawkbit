8200ca6da1496c8235a5282c677ceed87534a38e
gah6kor <gaurav.sahay@in.bosch.com>
2016-07-18 11:08:23 +0200

    Formatted the code

--- hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/CustomCommonDialogWindow.java
+++ hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/CustomCommonDialogWindow.java
@@ -10,17 +10,16 @@ package org.eclipse.hawkbit.ui.common;
 
 import org.eclipse.hawkbit.ui.utils.I18N;
 
-import com.vaadin.event.FieldEvents.BlurListener;
 import com.vaadin.ui.AbstractLayout;
-import com.vaadin.ui.Component;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
 
 public class CustomCommonDialogWindow extends CommonDialogWindow {
 	private static final long serialVersionUID = -4453608850403359992L;
 
 	public CustomCommonDialogWindow(final String caption, final Component content, final String helpLink,
 			final ClickListener saveButtonClickListener, final ClickListener cancelButtonClickListener,
-			final AbstractLayout layout, final I18N i18n) {
+			final AbstractLayout layout, final I18N i18n) { 
 		super(caption, content, helpLink, saveButtonClickListener, cancelButtonClickListener, layout, i18n);
 	}
 	
