78e536a484ccd066ae60bb12b94e1765a0d30c99
SirWayne <dennis.melzer@bosch-si.com>
2016-09-13 12:48:51 +0200

    Restructur cache evict events
    Signed-off-by: SirWayne <dennis.melzer@bosch-si.com>

---
+++ hawkbit-repository/hawkbit-repository-api/src/main/java/org/springframework/cloud/bus/event/CacheClearedEvent.java
@@ -0,0 +1,35 @@
+package org.springframework.cloud.bus.event;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class CacheClearedEvent extends AbstractDistributedEvent {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final int NO_REVISION = -1;
+
+    private final String cacheName;
+
+    /**
+     * Constructor.
+     *
+     * @param tanent
+     *            the tenant for this event
+     * @param cacheName
+     *            the name of the cache which was cleared
+     */
+    public CacheClearedEvent(final String tanent, final String cacheName) {
+        super(NO_REVISION, tanent);
+        this.cacheName = checkNotNull(cacheName);
+    }
+
+    public String getCacheName() {
+        return cacheName;
+    }
+
+    @Override
+    public String getTenant() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
