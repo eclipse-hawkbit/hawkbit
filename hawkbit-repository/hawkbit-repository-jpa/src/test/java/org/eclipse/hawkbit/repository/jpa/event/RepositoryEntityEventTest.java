429d308154f3e9ff31bb1ffefb4ce6ced4081900
SirWayne <dennis.melzer@bosch-si.com>
2016-09-15 15:15:18 +0200

    Move event testing class into event package
    Signed-off-by: SirWayne <dennis.melzer@bosch-si.com>

--- hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/event/RepositoryEntityEventTest.java
+++ hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/event/RepositoryEntityEventTest.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.hawkbit.repository.jpa.eventbus;
+package org.eclipse.hawkbit.repository.jpa.event;
 
 import static org.fest.assertions.Assertions.assertThat;
 
