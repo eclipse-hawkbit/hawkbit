8a85e88ee54a23d8cd50d5d11ed36649ba649cfd
SirWayne <dennis.melzer@bosch-si.com>
2016-09-15 15:03:18 +0200

    Move the EventPublisherAutoConfiguration in the event package.
    Signed-off-by: SirWayne <dennis.melzer@bosch-si.com>

--- hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/event/EventPublisherAutoConfiguration.java
+++ hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/event/EventPublisherAutoConfiguration.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.hawkbit.autoconfigure.eventbus;
+package org.eclipse.hawkbit.autoconfigure.event;
 
 import java.util.concurrent.Executor;
 
