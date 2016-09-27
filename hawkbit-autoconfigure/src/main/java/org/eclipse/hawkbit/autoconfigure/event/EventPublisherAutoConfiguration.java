0ece91ebc993919512772b811f38a666dd9b6e0f
SirWayne <dennis.melzer@bosch-si.com>
2016-09-27 16:22:39 +0200

    A remote event should execute in the tenant aware context
    Signed-off-by: SirWayne <dennis.melzer@bosch-si.com>

--- hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/event/EventPublisherAutoConfiguration.java
+++ hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/event/EventPublisherAutoConfiguration.java
@@ -10,14 +10,19 @@ package org.eclipse.hawkbit.autoconfigure.event;
 
 import java.util.concurrent.Executor;
 
+import org.eclipse.hawkbit.repository.event.remote.TenantAwareDistributedEvent;
 import org.eclipse.hawkbit.repository.jpa.model.helper.EventPublisherHolder;
+import org.eclipse.hawkbit.tenancy.TenantAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.bus.ServiceMatcher;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.ResolvableType;
 
 /**
  * Auto configuration for the event bus.
@@ -30,6 +35,9 @@ public class EventPublisherAutoConfiguration {
     @Qualifier("asyncExecutor")
     private Executor executor;
 
+    @Autowired
+    private TenantAware tenantAware;
+
     /**
      * Server internal event publisher that allows parallel event processing if
      * the event listener is marked as so.
@@ -37,9 +45,9 @@ public class EventPublisherAutoConfiguration {
      * @return publisher bean
      */
     @Bean(name = AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME)
-    @ConditionalOnMissingBean
-    public SimpleApplicationEventMulticaster applicationEventMulticaster() {
-        final SimpleApplicationEventMulticaster simpleApplicationEventMulticaster = new SimpleApplicationEventMulticaster();
+    public ApplicationEventMulticaster applicationEventMulticaster() {
+        final SimpleApplicationEventMulticaster simpleApplicationEventMulticaster = new TenantAwareApplicationEventPublisher(
+                tenantAware);
         simpleApplicationEventMulticaster.setTaskExecutor(executor);
         return simpleApplicationEventMulticaster;
     }
@@ -50,6 +58,44 @@ public class EventPublisherAutoConfiguration {
     @Bean
     public EventPublisherHolder eventBusHolder() {
         return EventPublisherHolder.getInstance();
+    }
+
+    private static class TenantAwareApplicationEventPublisher extends SimpleApplicationEventMulticaster {
+
+        private final TenantAware tenantAware;
+
+        @Autowired(required = false)
+        private ServiceMatcher serviceMatcher;
+
+        /**
+         * Constructor.
+         * 
+         * @param tenantAware
+         *            the tenant ware
+         */
+        protected TenantAwareApplicationEventPublisher(final TenantAware tenantAware) {
+            this.tenantAware = tenantAware;
+        }
+
+        @Override
+        public void multicastEvent(final ApplicationEvent event, final ResolvableType eventType) {
+            if (serviceMatcher == null || !(event instanceof TenantAwareDistributedEvent)) {
+                super.multicastEvent(event, eventType);
+                return;
+            }
+            final TenantAwareDistributedEvent remoteEvent = (TenantAwareDistributedEvent) event;
+
+            if (serviceMatcher.isFromSelf(remoteEvent)) {
+                super.multicastEvent(event, eventType);
+                return;
+            }
+
+            tenantAware.runAsTenant(remoteEvent.getTenant(), () -> {
+                super.multicastEvent(event, eventType);
+                return null;
+            });
+        }
+
     }
 
 }
