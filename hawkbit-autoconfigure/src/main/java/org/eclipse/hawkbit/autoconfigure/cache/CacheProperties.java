24497f586e84b8dddff6688231bbaec0ac330ec3
Jonathan Philip Knoblauch <JonathanPhilip.Knoblauch@bosch-si.com>
2016-10-18 10:55:09 +0200

    Added more javedoc and remove readme.md doku that is not needed anymore
    Signed-off-by: Jonathan Philip Knoblauch <JonathanPhilip.Knoblauch@bosch-si.com>

--- hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/cache/CacheProperties.java
+++ hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/cache/CacheProperties.java
@@ -7,7 +7,9 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Properties for configuring a cluster.
+ * Properties for configuring the cache within a cluster. The TTL (time to live)
+ * is used for the lifetime limit of data in caches. After lifetime the data
+ * gets reloaded out of the database.
  */
 @ConfigurationProperties("hawkbit.cache.global")
 public class CacheProperties {
