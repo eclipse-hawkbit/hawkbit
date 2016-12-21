ea611ae421b8d49cb81bb501a8842876f73da901
kaizimmerm <kai.zimmermann@bosch-si.com>
2016-12-21 10:44:50 +0100

    Readded line that was remove by incident.
    Signed-off-by: kaizimmerm <kai.zimmermann@bosch-si.com>

--- hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/management/targettag/CustomTargetTagFilterButtonClick.java
+++ hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/management/targettag/CustomTargetTagFilterButtonClick.java
@@ -11,6 +11,7 @@ package org.eclipse.hawkbit.ui.management.targettag;
 import java.io.Serializable;
 
 import org.eclipse.hawkbit.repository.TargetFilterQueryManagement;
+import org.eclipse.hawkbit.repository.model.TargetFilterQuery;
 import org.eclipse.hawkbit.ui.common.filterlayout.AbstractFilterSingleButtonClick;
 import org.eclipse.hawkbit.ui.management.event.TargetFilterEvent;
 import org.eclipse.hawkbit.ui.management.state.ManagementUIState;
@@ -50,7 +51,9 @@ public class CustomTargetTagFilterButtonClick extends AbstractFilterSingleButton
 
     @Override
     protected void filterClicked(final Button clickedButton) {
-        this.managementUIState.getTargetTableFilters().setTargetFilterQuery((Long) clickedButton.getData());
+        final TargetFilterQuery targetFilterQuery = this.targetFilterQueryManagement
+                .findTargetFilterQueryById((Long) clickedButton.getData());
+        this.managementUIState.getTargetTableFilters().setTargetFilterQuery(targetFilterQuery.getId());
         this.eventBus.publish(this, TargetFilterEvent.FILTER_BY_TARGET_FILTER_QUERY);
     }
 
