ad8e27671c5f16e8d781fbb416bb905d1882de5e
SirWayne <dennis.melzer@bosch-si.com>
2016-12-30 18:13:05 +0100

    Restructure the autoconfigure module
    Signed-off-by: SirWayne <dennis.melzer@bosch-si.com>

--- hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/dmf/amqp/DmfApiAutoConfiguration.java
+++ hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/dmf/amqp/DmfApiAutoConfiguration.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.hawkbit.autoconfigure.amqp;
+package org.eclipse.hawkbit.autoconfigure.dmf.amqp;
 
 import org.eclipse.hawkbit.amqp.DmfApiConfiguration;
 import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
--- hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/mgmt/MgmtApiAutoConfiguration.java
+++ hawkbit-autoconfigure/src/main/java/org/eclipse/hawkbit/autoconfigure/mgmt/MgmtApiAutoConfiguration.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.hawkbit.autoconfigure.web;
+package org.eclipse.hawkbit.autoconfigure.mgmt;
 
 import org.eclipse.hawkbit.mgmt.rest.resource.MgmtApiConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
