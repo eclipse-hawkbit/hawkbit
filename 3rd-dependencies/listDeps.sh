e39aec640a48e751027079a63bdea5ddf5d96190
kaizimmerm <kai.zimmermann@bosch-si.com>
2017-03-24 13:20:46 +0100

    Revert accidental check in
    Signed-off-by: kaizimmerm <kai.zimmermann@bosch-si.com>

--- 3rd-dependencies/listDeps.sh
+++ 3rd-dependencies/listDeps.sh
@@ -8,7 +8,7 @@
 # http://www.eclipse.org/legal/epl-v10.html
 #
 cd ..
-mvn dependency:list -DexcludeGroupIds=org.eclipse -Dsort=true -DoutputFile=dependencies.txt
+mvn dependency:list -DexcludeGroupIds=org.eclipse.hawkbit -Dsort=true -DoutputFile=dependencies.txt
 find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:compile'|sort|uniq > 3rd-dependencies/compile.txt
 find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:test'|sort|uniq > 3rd-dependencies/test.txt
 find . -name dependencies.txt|while read i; do cat $i;done|grep '.*:.*:provided'|sort|uniq > 3rd-dependencies/provided.txt
