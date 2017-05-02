02ab2ca06a2e36fe0196e296d4564d252262b6e9
kaizimmerm <kai.zimmermann@bosch-si.com>
2017-05-02 08:59:56 +0200

    Add stream aware error controller.
    Signed-off-by: kaizimmerm <kai.zimmermann@bosch-si.com>

---
+++ hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/StreamAwareErrorController.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2015 Bosch Software Innovations GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.hawkbit.app;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.web.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Error page controller that ensures that ocet stream does not return text in
+ * case of an error.
+ *
+ */
+public class StreamAwareErrorController extends BasicErrorController {
+
+    /**
+     * A new {@link StreamAwareErrorController}.
+     * 
+     * @param errorAttributes
+     *            the error attributes
+     * @param errorProperties
+     *            configuration properties
+     */
+    public StreamAwareErrorController(final ErrorAttributes errorAttributes, final ErrorProperties errorProperties) {
+        super(errorAttributes, errorProperties);
+    }
+
+    @RequestMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    ResponseEntity<Void> errorStream(final HttpServletRequest request, final HttpServletResponse response) {
+        final HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(status);
+    }
+
+}
