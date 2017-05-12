a37180a4286cb9031b41bf7011ae2c34bc4a8226
Melanie Retter <melanie.retter@bosch-si.com>
2017-05-12 12:26:36 +0200

    ID of distribution tag buttons should not contain whitespaces
    Signed-off-by: Melanie Retter <melanie.retter@bosch-si.com>

--- hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/management/dstag/DistributionTagButtons.java
+++ hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/management/dstag/DistributionTagButtons.java
@@ -27,6 +27,7 @@ import org.eclipse.hawkbit.ui.utils.SPUILabelDefinitions;
 import org.eclipse.hawkbit.ui.utils.UIComponentIdProvider;
 import org.eclipse.hawkbit.ui.utils.UINotification;
 import org.eclipse.hawkbit.ui.utils.VaadinMessageSource;
+import org.springframework.util.StringUtils;
 import org.vaadin.addons.lazyquerycontainer.BeanQueryFactory;
 import org.vaadin.addons.lazyquerycontainer.LazyQueryContainer;
 import org.vaadin.spring.events.EventBus.UIEventBus;
@@ -94,7 +95,7 @@ public class DistributionTagButtons extends AbstractFilterButtons implements Ref
 
     @Override
     protected String createButtonId(final String name) {
-        return name;
+        return StringUtils.trimAllWhitespace(name);
     }
 
     @Override
