c4f6e0faa3fdf522454c7036501601b7a4c6de9e
kaizimmerm <kai.zimmermann@bosch-si.com>
2017-09-05 17:00:42 +0200

    Complete javadoc.
    Signed-off-by: kaizimmerm <kai.zimmermann@bosch-si.com>

--- hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/RepositoryManagement.java
+++ hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/RepositoryManagement.java
@@ -16,15 +16,11 @@ import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.NotNull;
 
 import org.eclipse.hawkbit.im.authentication.SpPermission.SpringEvalExpressions;
-import org.eclipse.hawkbit.repository.builder.SoftwareModuleTypeCreate;
-import org.eclipse.hawkbit.repository.builder.SoftwareModuleTypeUpdate;
 import org.eclipse.hawkbit.repository.exception.EntityNotFoundException;
 import org.eclipse.hawkbit.repository.exception.EntityReadOnlyException;
 import org.eclipse.hawkbit.repository.exception.RSQLParameterSyntaxException;
 import org.eclipse.hawkbit.repository.exception.RSQLParameterUnsupportedFieldException;
 import org.eclipse.hawkbit.repository.model.BaseEntity;
-import org.eclipse.hawkbit.repository.model.DistributionSet;
-import org.eclipse.hawkbit.repository.model.DistributionSetType;
 import org.eclipse.hawkbit.repository.model.SoftwareModuleType;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.domain.Page;
@@ -42,7 +38,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
  * @param <U>
  *            entity update builder
  */
-// FIXME: generic descriptions and exception
 public interface RepositoryManagement<T, C, U> {
 
     /**
@@ -68,7 +63,7 @@ public interface RepositoryManagement<T, C, U> {
      * 
      * @throws ConstraintViolationException
      *             if fields are not filled as specified. Check
-     *             {@link SoftwareModuleTypeCreate} for field constraints.
+     *             {@link BaseEntity} for field constraints.
      */
     @PreAuthorize(SpringEvalExpressions.HAS_AUTH_CREATE_REPOSITORY)
     T create(@NotNull C create);
@@ -82,21 +77,20 @@ public interface RepositoryManagement<T, C, U> {
      * @return updated Entity
      * 
      * @throws EntityReadOnlyException
-     *             if the {@link DistributionSetType} is already in use by a
-     *             {@link DistributionSet} and user tries to change list of
-     *             {@link SoftwareModuleType}s
+     *             if the {@link BaseEntity} cannot be updated (e.g. is already
+     *             in use)
      * @throws EntityNotFoundException
-     *             in case the {@link SoftwareModuleType} does not exists and
-     *             cannot be updated
+     *             in case the {@link BaseEntity} does not exists and cannot be
+     *             updated
      * @throws ConstraintViolationException
      *             if fields are not filled as specified. Check
-     *             {@link SoftwareModuleTypeUpdate} for field constraints.
+     *             {@link BaseEntity} for field constraints.
      */
     @PreAuthorize(SpringEvalExpressions.HAS_AUTH_UPDATE_REPOSITORY)
     T update(@NotNull U update);
 
     /**
-     * @return number of {@link SoftwareModuleType}s in the repository.
+     * @return number of {@link BaseEntity}s in the repository.
      */
     @PreAuthorize(SpringEvalExpressions.HAS_AUTH_READ_REPOSITORY)
     long count();
@@ -108,7 +102,7 @@ public interface RepositoryManagement<T, C, U> {
      *            to delete
      * 
      * @throws EntityNotFoundException
-     *             not found is type with given ID does not exist
+     *             BaseEntity with given ID does not exist
      */
     @PreAuthorize(SpringEvalExpressions.HAS_AUTH_DELETE_REPOSITORY)
     void delete(@NotNull Long id);
@@ -132,12 +126,14 @@ public interface RepositoryManagement<T, C, U> {
      *
      * @param ids
      *            the ids to for
-     * @return the found {@link DistributionSet}s
+     * @return the found {@link BaseEntity}s
      */
     @PreAuthorize(SpringEvalExpressions.HAS_AUTH_READ_REPOSITORY)
     List<T> get(@NotEmpty Collection<Long> ids);
 
     /**
+     * Verifies that {@link BaseEntity} with given ID exists in the repository.
+     * 
      * @param id
      *            of entity to check existence
      * @return <code>true</code> if entity with given ID exists
@@ -150,8 +146,8 @@ public interface RepositoryManagement<T, C, U> {
      *
      * @param id
      *            to search for
-     * @return {@link SoftwareModuleType} in the repository with given
-     *         {@link SoftwareModuleType#getId()}
+     * @return {@link BaseEntity} in the repository with given
+     *         {@link BaseEntity#getId()}
      */
     @PreAuthorize(SpringEvalExpressions.HAS_AUTH_READ_REPOSITORY)
     Optional<T> get(@NotNull Long id);
@@ -160,21 +156,21 @@ public interface RepositoryManagement<T, C, U> {
      * Retrieves {@link Page} of all {@link BaseEntity} of given type.
      * 
      * @param pageable
-     *            parameter
-     * @return all {@link SoftwareModuleType}s in the repository.
+     *            paging parameter
+     * @return all {@link BaseEntity}s in the repository.
      */
     @PreAuthorize(SpringEvalExpressions.HAS_AUTH_READ_REPOSITORY)
     Slice<T> findAll(@NotNull Pageable pageable);
 
     /**
-     * Retrieves all {@link SoftwareModuleType}s with a given specification.
+     * Retrieves all {@link BaseEntity}s with a given specification.
      * 
      * @param pageable
      *            pagination parameter
      * @param rsqlParam
      *            filter definition in RSQL syntax
      *
-     * @return the found {@link SoftwareModuleType}s
+     * @return the found {@link BaseEntity}s
      * 
      * @throws RSQLParameterUnsupportedFieldException
      *             if a field in the RSQL string is used but not provided by the
