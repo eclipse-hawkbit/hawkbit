2706646faae05b53b0d3186890280308cdabbe2a
kaizimmerm <kai.zimmermann@bosch-si.com>
2018-03-22 15:57:46 +0100

    Migrated common art repo funtions into abstract class.
    Signed-off-by: kaizimmerm <kai.zimmermann@bosch-si.com>

--- hawkbit-artifact-repository-filesystem/src/main/java/org/eclipse/hawkbit/artifact/repository/ArtifactFilesystemRepository.java
+++ hawkbit-artifact-repository-filesystem/src/main/java/org/eclipse/hawkbit/artifact/repository/ArtifactFilesystemRepository.java
@@ -8,17 +8,10 @@
  */
 package org.eclipse.hawkbit.artifact.repository;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.DigestOutputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -29,8 +22,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 
 import com.google.common.base.Splitter;
-import com.google.common.io.BaseEncoding;
-import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 
 /**
@@ -46,12 +37,10 @@ import com.google.common.io.Files;
  * SHA1-hash {@code (/basepath/[two digit sha1]/[two digit sha1])}.
  */
 @Validated
-public class ArtifactFilesystemRepository implements ArtifactRepository {
+public class ArtifactFilesystemRepository extends AbstractArtifactRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(ArtifactFilesystemRepository.class);
 
-    private static final String TEMP_FILE_PREFIX = "tmp";
-    private static final String TEMP_FILE_SUFFIX = "artifactrepo";
     private final ArtifactFilesystemProperties artifactResourceProperties;
 
     /**
@@ -63,33 +52,6 @@ public class ArtifactFilesystemRepository implements ArtifactRepository {
      */
     public ArtifactFilesystemRepository(final ArtifactFilesystemProperties artifactResourceProperties) {
         this.artifactResourceProperties = artifactResourceProperties;
-    }
-
-    @Override
-    public ArtifactFilesystem store(final String tenant, final InputStream content, final String filename,
-            final String contentType) {
-        return store(tenant, content, filename, contentType, null);
-    }
-
-    @Override
-    // suppress warning, of not strong enough hashing algorithm, SHA-1 and MD5
-    // is not used security related
-    @SuppressWarnings("squid:S2070")
-    public ArtifactFilesystem store(final String tenant, final InputStream content, final String filename,
-            final String contentType, final DbArtifactHash hash) {
-
-        final MessageDigest mdSHA1;
-        final MessageDigest mdMD5;
-        try {
-            mdSHA1 = MessageDigest.getInstance("SHA1");
-            mdMD5 = MessageDigest.getInstance("MD5");
-        } catch (final NoSuchAlgorithmException e) {
-            throw new ArtifactStoreException(e.getMessage(), e);
-        }
-
-        final File file = createTempFile();
-        final AbstractDbArtifact artifact = store(content, contentType, hash, mdSHA1, mdMD5, file);
-        return renameFileToSHA1Naming(tenant, file, artifact);
     }
 
     @Override
@@ -107,75 +69,24 @@ public class ArtifactFilesystemRepository implements ArtifactRepository {
         return new ArtifactFilesystem(file, sha1, new DbArtifactHash(sha1, null), file.length(), null);
     }
 
-    private AbstractDbArtifact store(final InputStream content, final String contentType, final DbArtifactHash hash,
-            final MessageDigest mdSHA1, final MessageDigest mdMD5, final File file) {
-        AbstractDbArtifact artifact;
-        try (final DigestOutputStream outputstream = openFileOutputStream(file, mdSHA1, mdMD5)) {
-            final long artifactSize = ByteStreams.copy(content, outputstream);
-            outputstream.flush();
-            final String sha1Hash = BaseEncoding.base16().lowerCase().encode(mdSHA1.digest());
-            final String md5Hash = BaseEncoding.base16().lowerCase().encode(mdMD5.digest());
-
-            artifact = new ArtifactFilesystem(file, sha1Hash, new DbArtifactHash(sha1Hash, md5Hash), artifactSize,
-                    contentType);
-
-            checkHashes(artifact, hash);
-        } catch (final IOException e) {
-            throw new ArtifactStoreException(e.getMessage(), e);
-        } catch (final HashNotMatchException e) {
-            if (!file.delete()) {
-                LOG.error("Could not delete temp file {}", file);
-            }
-            throw e;
-        }
-        return artifact;
+    @Override
+    protected AbstractDbArtifact store(final String tenant, final String sha1Hash16, final String mdMD5Hash16,
+            final String contentType, final File file) throws IOException {
+        return renameFileToSHA1Naming(tenant, file, new ArtifactFilesystem(file, sha1Hash16,
+                new DbArtifactHash(sha1Hash16, mdMD5Hash16), file.length(), contentType));
     }
 
     private ArtifactFilesystem renameFileToSHA1Naming(final String tenant, final File file,
-            final AbstractDbArtifact artifact) {
+            final AbstractDbArtifact artifact) throws IOException {
         final File fileSHA1Naming = getFile(tenant, artifact.getHashes().getSha1());
-        final ArtifactFilesystem fileSystemArtifact = new ArtifactFilesystem(fileSHA1Naming, artifact.getArtifactId(),
-                artifact.getHashes(), artifact.getSize(), artifact.getContentType());
         if (fileSHA1Naming.exists()) {
             FileUtils.deleteQuietly(file);
         } else {
-            try {
-                Files.move(file, fileSHA1Naming);
-            } catch (final IOException e) {
-                throw new ArtifactStoreException("Could not store the file " + fileSHA1Naming, e);
-            }
+            Files.move(file, fileSHA1Naming);
         }
 
-        if (!file.delete()) {
-            LOG.debug("Could not delete temp file {}", file);
-        }
-
-        return fileSystemArtifact;
-    }
-
-    private AbstractDbArtifact checkHashes(final AbstractDbArtifact artifact, final DbArtifactHash hash) {
-        if (hash == null) {
-            return artifact;
-        }
-        if (hash.getSha1() != null && !artifact.getHashes().getSha1().equals(hash.getSha1())) {
-            throw new HashNotMatchException("The given sha1 hash " + hash.getSha1()
-                    + " does not match with the calcualted sha1 hash " + artifact.getHashes().getSha1(),
-                    HashNotMatchException.SHA1);
-        }
-        if (hash.getMd5() != null && !artifact.getHashes().getMd5().equals(hash.getMd5())) {
-            throw new HashNotMatchException("The given md5 hash " + hash.getMd5()
-                    + " does not match with the calcualted md5 hash " + artifact.getHashes().getMd5(),
-                    HashNotMatchException.MD5);
-        }
-        return artifact;
-    }
-
-    private File createTempFile() {
-        try {
-            return File.createTempFile(TEMP_FILE_PREFIX, TEMP_FILE_SUFFIX);
-        } catch (final IOException e) {
-            throw new ArtifactStoreException("Cannot create tempfile", e);
-        }
+        return new ArtifactFilesystem(fileSHA1Naming, artifact.getArtifactId(), artifact.getHashes(),
+                artifact.getSize(), artifact.getContentType());
     }
 
     private File getFile(final String tenant, final String sha1) {
@@ -192,18 +103,9 @@ public class ArtifactFilesystemRepository implements ArtifactRepository {
         return Paths.get(artifactResourceProperties.getPath(), sanitizeTenant(tenant), folder1, folder2);
     }
 
-    private DigestOutputStream openFileOutputStream(final File file, final MessageDigest mdSHA1,
-            final MessageDigest mdMD5) throws FileNotFoundException {
-        return new DigestOutputStream(
-                new DigestOutputStream(new BufferedOutputStream(new FileOutputStream(file)), mdMD5), mdSHA1);
-    }
-
     @Override
     public void deleteByTenant(final String tenant) {
         FileUtils.deleteQuietly(Paths.get(artifactResourceProperties.getPath(), sanitizeTenant(tenant)).toFile());
     }
 
-    private static String sanitizeTenant(final String tenant) {
-        return tenant.trim().toUpperCase();
-    }
 }
