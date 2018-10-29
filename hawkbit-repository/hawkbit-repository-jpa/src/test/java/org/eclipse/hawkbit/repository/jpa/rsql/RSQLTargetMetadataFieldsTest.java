43a40c0a0509db3fa1416c8e53f2af61a7b5967f
Stefan Behl <stefan.behl@bosch-si.com>
2018-10-29 09:08:03 +0100

    Fix Allure imports and annotations
    Signed-off-by: Stefan Behl <stefan.behl@bosch-si.com>

--- hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/rsql/RSQLTargetMetadataFieldsTest.java
+++ hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/rsql/RSQLTargetMetadataFieldsTest.java
@@ -24,12 +24,12 @@ import org.junit.Test;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
-import ru.yandex.qatools.allure.annotations.Description;
-import ru.yandex.qatools.allure.annotations.Features;
-import ru.yandex.qatools.allure.annotations.Stories;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 
-@Features("Component Tests - Repository")
-@Stories("RSQL filter target metadata")
+@Feature("Component Tests - Repository")
+@Story("RSQL filter target metadata")
 public class RSQLTargetMetadataFieldsTest extends AbstractJpaIntegrationTest {
     private String controllerId;
 
