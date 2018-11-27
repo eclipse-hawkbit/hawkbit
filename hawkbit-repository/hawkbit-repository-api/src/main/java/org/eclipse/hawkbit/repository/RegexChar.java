c70720e546d716b49a4016253cea7db96e2bfe69
Stefan Klotz <stefan.klotz@bosch-si.com>
2018-11-27 10:24:35 +0100

    add regular expression classes
    Signed-off-by: Stefan Klotz <stefan.klotz@bosch-si.com>

---
+++ hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/RegexChar.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2015 Bosch Software Innovations GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.hawkbit.repository;
+
+/**
+ * Regular expression characters providing their encoded and their readable
+ * expression
+ */
+public class RegexChar {
+
+    public static RegexChar WHITESPACE = new RegexChar("/s", "whitespace");
+    public static RegexChar DIGITS = new RegexChar("0-9", "digits");
+    public static RegexChar QUOTATION_MARKS = new RegexChar("'\"", "quotation marks");
+    public static RegexChar SLASHES = new RegexChar("\\/\\\\", "slashes");
+    public static RegexChar GREATER_THAN = new RegexChar(">");
+    public static RegexChar LESS_THAN = new RegexChar("<");
+    public static RegexChar EQUALS = new RegexChar("=");
+    public static RegexChar EXCLAMATION_MARK = new RegexChar("!");
+    public static RegexChar QUESRION_MARK = new RegexChar("?");
+    
+    public final String regExp;
+    public final String readableExp;
+
+    private RegexChar(final String character) {
+        this(character, character);
+    }
+
+    private RegexChar(final String regExp, final String readableExp) {
+        this.regExp = regExp;
+        this.readableExp = readableExp;
+    }
+
+    @Override
+    public boolean equals(final Object obj){
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RegexChar other = (RegexChar) obj;
+        return this.hashCode() == other.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return readableExp;
+    }
+
+    @Override
+    public int hashCode() {
+        return regExp.hashCode();
+    }
+
+}
---
+++ hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/RegexHelper.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2015 Bosch Software Innovations GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.hawkbit.repository;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Helper to evaluate regular expressions defined by {@link RegexChar}.
+ */
+public class RegexHelper {
+
+    public static boolean stringContainsIllegalCharacters(final String stringToCheck,
+            final List<RegexChar> illegalChars) {
+        final StringBuilder charBuilder = new StringBuilder();
+        illegalChars.forEach(character -> charBuilder.append(character.regExp));
+        final String regularExpressions = String.format("[%s]", charBuilder.toString());
+        return !Pattern.matches(regularExpressions, stringToCheck);
+    }
+
+}
