d89df5a1c3b09b2f2b24fcc7ff44b6a4512b63b0
Brandon Schmitt <Brandon.Schmitt@kiwigrid.com>
2019-09-05 13:05:24 +0200

    Refactor Hono Configuration
    Signed-off-by: Brandon Schmitt <Brandon.Schmitt@kiwigrid.com>

--- hawkbit-dmf/hawkbit-dmf-hono/src/main/java/org/eclipse/hawkbit/dmf/hono/DmfHonoConfiguration.java
+++ hawkbit-dmf/hawkbit-dmf-hono/src/main/java/org/eclipse/hawkbit/dmf/hono/DmfHonoConfiguration.java
@@ -1,41 +1,90 @@
 package org.eclipse.hawkbit.dmf.hono;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "hawkbit.dmf.hono")
 @ComponentScan
 public class DmfHonoConfiguration {
-
-    @Value("${hawkbit.server.repository.hono-sync.tenant-list-uri}")
-    private String honoTenantListUri;
-
-    @Value("${hawkbit.server.repository.hono-sync.device-list-uri}")
-    private String honoDeviceListUri;
-
-    @Value("${hawkbit.server.repository.hono-sync.credentials-list-uri}")
-    private String honoCredentialsListUri;
-
-    @Value("${hawkbit.server.repository.hono-sync.authorization-method:none}")
-    private String authorizationMethod;
-
-    @Value("${hawkbit.server.repository.hono-sync.oidc-token-uri:}")
-    private String oidcTokenUri;
-
-    @Value("${hawkbit.server.repository.hono-sync.oidc-client-id:}")
-    private String oidcClientId;
-
-    @Value("${hawkbit.server.repository.hono-sync.user.name:}")
-    private String username;
-
-    @Value("${hawkbit.server.repository.hono-sync.user.password:}")
-    private String password;
+    private String tenantListUri;
+    private String deviceListUri;
+    private String credentialsListUri;
+    private String authorizationMethod = "none";
+    private String oidcTokenUri = "";
+    private String oidcClientId = "";
+    private String username = "";
+    private String password = "";
 
     @Bean
     public HonoDeviceSync honoDeviceSync() {
-        return new HonoDeviceSync(honoTenantListUri, honoDeviceListUri, honoCredentialsListUri, authorizationMethod,
+        return new HonoDeviceSync(tenantListUri, deviceListUri, credentialsListUri, authorizationMethod,
                 oidcTokenUri, oidcClientId, username, password);
+    }
+
+    public String getTenantListUri() {
+        return tenantListUri;
+    }
+
+    public void setTenantListUri(String tenantListUri) {
+        this.tenantListUri = tenantListUri;
+    }
+
+    public String getDeviceListUri() {
+        return deviceListUri;
+    }
+
+    public void setDeviceListUri(String deviceListUri) {
+        this.deviceListUri = deviceListUri;
+    }
+
+    public String getCredentialsListUri() {
+        return credentialsListUri;
+    }
+
+    public void setCredentialsListUri(String credentialsListUri) {
+        this.credentialsListUri = credentialsListUri;
+    }
+
+    public String getAuthorizationMethod() {
+        return authorizationMethod;
+    }
+
+    public void setAuthorizationMethod(String authorizationMethod) {
+        this.authorizationMethod = authorizationMethod;
+    }
+
+    public String getOidcTokenUri() {
+        return oidcTokenUri;
+    }
+
+    public void setOidcTokenUri(String oidcTokenUri) {
+        this.oidcTokenUri = oidcTokenUri;
+    }
+
+    public String getOidcClientId() {
+        return oidcClientId;
+    }
+
+    public void setOidcClientId(String oidcClientId) {
+        this.oidcClientId = oidcClientId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
