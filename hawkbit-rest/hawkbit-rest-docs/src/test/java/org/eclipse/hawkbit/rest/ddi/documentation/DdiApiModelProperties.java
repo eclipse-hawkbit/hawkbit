31da3e3d850a91bfd4b2267d984083d9e8093371
Robert Sing <Robert.Sing@bosch-si.com>
2019-10-09 08:31:50 +0200

    improved formatting
    Signed-off-by: Robert Sing <Robert.Sing@bosch-si.com>

--- hawkbit-rest/hawkbit-rest-docs/src/test/java/org/eclipse/hawkbit/rest/ddi/documentation/DdiApiModelProperties.java
+++ hawkbit-rest/hawkbit-rest-docs/src/test/java/org/eclipse/hawkbit/rest/ddi/documentation/DdiApiModelProperties.java
@@ -108,7 +108,9 @@ final class DdiApiModelProperties {
 
     static final String FILENAME = "file name of artifact";
 
-    static final String TARGET_CONFIG_DATA = "Link which is provided whenever the provisioning target or device is supposed to push its configuration data (aka. \"controller attributes\") to the server. Only shown for the initial configuration, after a successful update action, or if requested explicitly (e.g. via the management UI).";
+    static final String TARGET_CONFIG_DATA = "Link which is provided whenever the provisioning target or device is supposed "
+                    + "to push its configuration data (aka. \"controller attributes\") to the server. Only shown for the initial "
+                    + "configuration, after a successful update action, or if requested explicitly (e.g. via the management UI).";
 
     static final String ARTIFACT_HASHES_SHA1 = "SHA1 hash of the artifact in Base 16 format";
     static final String ARTIFACT_HASHES_MD5 = "MD5 hash of the artifact";
