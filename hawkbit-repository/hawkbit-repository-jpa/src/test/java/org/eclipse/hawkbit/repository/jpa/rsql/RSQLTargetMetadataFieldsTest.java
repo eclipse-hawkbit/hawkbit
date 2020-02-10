a9743810dcff8031c519fd7b3dac3dc799e4453e
Sebastian Firsching <sebastian.firsching@bosch-si.com>
2020-02-10 15:06:17 +0100

    Adapt missing RSQL metadata test
    Signed-off-by: Sebastian Firsching <sebastian.firsching@bosch-si.com>

--- hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/rsql/RSQLTargetMetadataFieldsTest.java
+++ hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/rsql/RSQLTargetMetadataFieldsTest.java
@@ -66,7 +66,7 @@ public class RSQLTargetMetadataFieldsTest extends AbstractJpaIntegrationTest {
         assertRSQLQuery(TargetMetadataFields.VALUE.name() + "==1", 1);
         assertRSQLQuery(TargetMetadataFields.VALUE.name() + "!=1", 5);
         assertRSQLQuery(TargetMetadataFields.VALUE.name() + "=in=(1,2)", 2);
-        assertRSQLQuery(TargetMetadataFields.VALUE.name() + "=out=(1,2)", 3);
+        assertRSQLQuery(TargetMetadataFields.VALUE.name() + "=out=(1,2)", 4);
     }
 
     private void assertRSQLQuery(final String rsqlParam, final long expectedEntities) {
