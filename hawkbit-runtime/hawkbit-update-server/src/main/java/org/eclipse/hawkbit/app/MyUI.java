71bb198ff2f6d7dfe13ce083461ffebe23d2554b
Bogdan Bondar <Bogdan.Bondar@bosch.io>
2020-09-03 10:03:08 +0200

    added comment why we used WEBSOCKET_XHR instead of WEBSOCKET
    Signed-off-by: Bogdan Bondar <Bogdan.Bondar@bosch.io>

--- hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/MyUI.java
+++ hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/MyUI.java
@@ -32,7 +32,9 @@ import com.vaadin.spring.navigator.SpringViewProvider;
  * A {@link SpringUI} annotated class must be present in the classpath. The
  * easiest way to get an hawkBit UI running is to extend the
  * {@link AbstractHawkbitUI} and to annotated it with {@link SpringUI} as in
- * this example.
+ * this example. WEBSOCKET_XHR transport is used instead of WEBSOCKET in order
+ * to preserve Spring Security Context, that does not work using websocket
+ * communication with Vaadin Shared Security.
  *
  */
 @SpringUI
