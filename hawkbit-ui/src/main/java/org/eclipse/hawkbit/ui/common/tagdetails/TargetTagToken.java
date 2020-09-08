c9b423ea888279883f862b683c70a125ef580f88
Bogdan Bondar <Bogdan.Bondar@bosch.io>
2020-09-08 12:32:11 +0200

    introduced paging while retrieving the target/ds tags
    Signed-off-by: Bogdan Bondar <Bogdan.Bondar@bosch.io>

--- hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/tagdetails/TargetTagToken.java
+++ hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/tagdetails/TargetTagToken.java
@@ -24,9 +24,9 @@ import org.eclipse.hawkbit.ui.common.data.proxies.ProxyTarget;
 import org.eclipse.hawkbit.ui.common.event.EntityModifiedEventPayload;
 import org.eclipse.hawkbit.ui.common.event.EntityModifiedEventPayload.EntityModifiedEventType;
 import org.eclipse.hawkbit.ui.common.event.EventTopics;
+import org.eclipse.hawkbit.ui.utils.HawkbitCommonUtil;
 import org.eclipse.hawkbit.ui.utils.UINotification;
 import org.eclipse.hawkbit.ui.utils.VaadinMessageSource;
-import org.springframework.data.domain.PageRequest;
 import org.vaadin.spring.events.EventBus.UIEventBus;
 
 /**
@@ -106,15 +106,15 @@ public class TargetTagToken extends AbstractTagToken<ProxyTarget> {
 
     @Override
     protected List<ProxyTag> getAllTags() {
-        return targetTagManagement.findAll(PageRequest.of(0, MAX_TAG_QUERY)).stream()
+        return HawkbitCommonUtil.getEntitiesByPageableProvider(targetTagManagement::findAll).stream()
                 .map(tag -> new ProxyTag(tag.getId(), tag.getName(), tag.getColour())).collect(Collectors.toList());
     }
 
     @Override
     protected List<ProxyTag> getAssignedTags() {
-        return getMasterEntity().map(masterEntity -> targetTagManagement
-                .findByTarget(PageRequest.of(0, MAX_TAG_QUERY), masterEntity.getControllerId()).stream()
-                .map(tagMapper::map).collect(Collectors.toList())).orElse(Collections.emptyList());
+        return getMasterEntity().map(masterEntity -> HawkbitCommonUtil
+                .getEntitiesByPageableProvider(p -> targetTagManagement.findByTarget(p, masterEntity.getControllerId()))
+                .stream().map(tagMapper::map).collect(Collectors.toList())).orElse(Collections.emptyList());
     }
 
     @Override
