d800c6e4ee7631546a650d6c3e3064efe5b5750b
Anand kumar <anand.kumar@bosch.io>
2021-06-09 10:58:00 +0200

    Added docs for the public methods in userinfo
    Signed-off-by: Anand kumar <anand.kumar@bosch.io>

--- hawkbit-rest/hawkbit-mgmt-api/src/main/java/org/eclipse/hawkbit/mgmt/json/model/auth/MgmtUserInfo.java
+++ hawkbit-rest/hawkbit-mgmt-api/src/main/java/org/eclipse/hawkbit/mgmt/json/model/auth/MgmtUserInfo.java
@@ -9,7 +9,7 @@
 package org.eclipse.hawkbit.mgmt.json.model.auth;
 
 /**
- * A json annotated rest model for User Auth to RESTful API representation.
+ * A json annotated rest model for Userinfo to RESTful API representation.
  *
  */
 public class MgmtUserInfo {
@@ -17,18 +17,30 @@ public class MgmtUserInfo {
     private String username;
     private String tenant;
 
+    /**
+     * @return Username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * @param username Username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * @return Tenant
+     */
     public String getTenant() {
         return tenant;
     }
 
+    /**
+     * @param tenant Tenant
+     */
     public void setTenant(String tenant) {
         this.tenant = tenant;
     }
