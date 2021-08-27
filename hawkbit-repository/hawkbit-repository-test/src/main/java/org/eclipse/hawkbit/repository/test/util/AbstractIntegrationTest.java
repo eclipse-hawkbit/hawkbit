c2b023df1874830c080876506797035727173a22
Bogdan Bondar <Bogdan.Bondar@bosch.io>
2021-08-27 10:15:02 +0200

    substituted deprecated guava create temp dir method
    Signed-off-by: Bogdan Bondar <Bogdan.Bondar@bosch.io>

--- hawkbit-repository/hawkbit-repository-test/src/main/java/org/eclipse/hawkbit/repository/test/util/AbstractIntegrationTest.java
+++ hawkbit-repository/hawkbit-repository-test/src/main/java/org/eclipse/hawkbit/repository/test/util/AbstractIntegrationTest.java
@@ -15,6 +15,7 @@ import static org.eclipse.hawkbit.im.authentication.SpPermission.SpringEvalExpre
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.Collections;
@@ -88,10 +89,8 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestExecutionListeners.MergeMode;
 import org.springframework.test.context.TestPropertySource;
 
-import com.google.common.io.Files;
-
 @ActiveProfiles({ "test" })
-@ExtendWith({JUnitTestLoggerExtension.class, WithSpringAuthorityRule.class})
+@ExtendWith({ JUnitTestLoggerExtension.class, WithSpringAuthorityRule.class })
 @WithUser(principal = "bumlux", allSpPermissions = true, authorities = { CONTROLLER_ROLE, SYSTEM_ROLE })
 @SpringBootTest
 @ContextConfiguration(classes = { TestConfiguration.class, TestSupportBinderAutoConfiguration.class })
@@ -383,8 +382,15 @@ public abstract class AbstractIntegrationTest {
 
     }
 
-    private static String artifactDirectory = Files.createTempDir().getAbsolutePath() + "/"
-            + RandomStringUtils.randomAlphanumeric(20);
+    private static String artifactDirectory = createTempDir();
+
+    private static String createTempDir() {
+        try {
+            return Files.createTempDirectory(null).toString() + "/" + RandomStringUtils.randomAlphanumeric(20);
+        } catch (final IOException e) {
+            throw new IllegalStateException("Failed to create temp directory");
+        }
+    }
 
     @AfterEach
     public void cleanUp() {
