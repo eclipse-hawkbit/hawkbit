6e499919e0377fda6426105e533ee8e82f73437f
Florian Ruschbaschan <Florian.Ruschbaschan@bosch.io>
2021-11-12 16:38:40 +0100

    Fix sql migration scripts
    Signed-off-by: Florian Ruschbaschan <Florian.Ruschbaschan@bosch.io>

--- hawkbit-repository/hawkbit-repository-jpa/src/main/resources/db/migration/SQL_SERVER/V1_12_20__add_encryption_flag_to_sm___SQL_SERVER.sql
+++ hawkbit-repository/hawkbit-repository-jpa/src/main/resources/db/migration/SQL_SERVER/V1_12_20__add_encryption_flag_to_sm___SQL_SERVER.sql
@@ -1,3 +1,1 @@
-ALTER TABLE sp_base_software_module ADD COLUMN encrypted BOOLEAN;
-
-UPDATE sp_base_software_module SET encrypted = 0;
+ALTER TABLE sp_base_software_module ADD encrypted BIT DEFAULT 0;
