527f7de6a8f9dadf6656ee80a8ba6a411fdd5f39
Bondar Bogdan <36962546+bogdan-bondar@users.noreply.github.com>
2022-03-25 16:00:18 +0100

    Fix overdue timestamp calculation permission (#1235)
    * fixed overdue timestamp calculation permission error
    
    Signed-off-by: Bogdan Bondar <Bogdan.Bondar@bosch.io>
    
    * changed method visibility
    
    Signed-off-by: Bogdan Bondar <Bogdan.Bondar@bosch.io>

--- hawkbit-repository/hawkbit-repository-core/src/main/java/org/eclipse/hawkbit/repository/model/helper/SystemSecurityContextHolder.java
+++ hawkbit-repository/hawkbit-repository-core/src/main/java/org/eclipse/hawkbit/repository/model/helper/SystemSecurityContextHolder.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.hawkbit.repository.jpa.model.helper;
+package org.eclipse.hawkbit.repository.model.helper;
 
 import org.eclipse.hawkbit.security.SystemSecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
