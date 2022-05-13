7e974951abf250fdf7e998f9ef03e392a93bff13
Bogdan Bondar <Bogdan.Bondar@bosch.io>
2022-05-13 16:45:36 +0200

    improved debug logs for autoassignment
    Signed-off-by: Bogdan Bondar <Bogdan.Bondar@bosch.io>

--- hawkbit-repository/hawkbit-repository-jpa/src/main/java/org/eclipse/hawkbit/repository/jpa/autoassign/AutoAssignChecker.java
+++ hawkbit-repository/hawkbit-repository-jpa/src/main/java/org/eclipse/hawkbit/repository/jpa/autoassign/AutoAssignChecker.java
@@ -65,37 +65,43 @@ public class AutoAssignChecker extends AbstractAutoAssignExecutor {
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void check() {
-        LOGGER.debug("Auto assigned check call");
-
+        LOGGER.debug("Auto assign check call for tenant {} started", getTenantAware().getCurrentTenant());
         forEachFilterWithAutoAssignDS(this::checkByTargetFilterQueryAndAssignDS);
+        LOGGER.debug("Auto assign check call for tenant {} finished", getTenantAware().getCurrentTenant());
     }
 
     /**
-     * Fetches the distribution set, gets all controllerIds and assigns the DS to
-     * them. Catches PersistenceException and own exceptions derived from
+     * Fetches the distribution set, gets all controllerIds and assigns the DS
+     * to them. Catches PersistenceException and own exceptions derived from
      * AbstractServerRtException
      *
      * @param targetFilterQuery
      *            the target filter query
      */
     private void checkByTargetFilterQueryAndAssignDS(final TargetFilterQuery targetFilterQuery) {
+        LOGGER.debug("Auto assign check call for tenant {} and target filter query id {} started",
+                getTenantAware().getCurrentTenant(), targetFilterQuery.getId());
         try {
             int count;
             do {
-
                 final List<String> controllerIds = targetManagement
                         .findByTargetFilterQueryAndNonDSAndCompatible(
                                 PageRequest.of(0, Constants.MAX_ENTRIES_IN_STATEMENT),
                                 targetFilterQuery.getAutoAssignDistributionSet().getId(), targetFilterQuery.getQuery())
                         .getContent().stream().map(Target::getControllerId).collect(Collectors.toList());
+                LOGGER.debug(
+                        "Retrieved {} auto assign targets for tenant {} and target filter query id {}, starting with assignment",
+                        controllerIds.size(), getTenantAware().getCurrentTenant(), targetFilterQuery.getId());
+
                 count = runTransactionalAssignment(targetFilterQuery, controllerIds);
-
+                LOGGER.debug(
+                        "Assignment for {} auto assign targets for tenant {} and target filter query id {} finished",
+                        controllerIds.size(), getTenantAware().getCurrentTenant(), targetFilterQuery.getId());
             } while (count == Constants.MAX_ENTRIES_IN_STATEMENT);
-
         } catch (final PersistenceException | AbstractServerRtException e) {
-            LOGGER.error("Error during auto assign check of target filter query " + targetFilterQuery.getId(), e);
+            LOGGER.error("Error during auto assign check of target filter query id {}", targetFilterQuery.getId(), e);
         }
-
+        LOGGER.debug("Auto assign check call for tenant {} and target filter query id {} finished",
+                getTenantAware().getCurrentTenant(), targetFilterQuery.getId());
     }
-
 }
