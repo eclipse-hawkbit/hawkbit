817037fe85211231966ab8a08f4316f252a8c024
Stefan Behl <stefan.behl@bosch.io>
2023-04-19 09:10:30 +0200

    Fix copyright statement

--- check-dependencies.sh
+++ check-dependencies.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 #*******************************************************************************
-# Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
+# Copyright (c) 2023 Bosch.IO GmbH and others.
 #
-# See the NOTICE file(s) distributed with this work for additional
-# information regarding copyright ownership.
-#
-# This program and the accompanying materials are made available under the
-# terms of the Eclipse Public License 2.0 which is available at
-# http://www.eclipse.org/legal/epl-2.0
-#
-# SPDX-License-Identifier: EPL-2.0
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
 #*******************************************************************************
 
 DASH_LICENSE_JAR=$1
