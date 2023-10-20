0e2d279714b96b1e6e15fd0f5fa9ca7782b06b03
Vasil Ilchev <vasil.ilchev@bosch.io>
2023-10-20 18:26:57 +0300

    REST API extend System Configuration to support Default DistributionSet Type

--- hawkbit-rest/hawkbit-mgmt-resource/src/main/java/org/eclipse/hawkbit/mgmt/rest/resource/MgmtTenantManagementResource.java
+++ hawkbit-rest/hawkbit-mgmt-resource/src/main/java/org/eclipse/hawkbit/mgmt/rest/resource/MgmtTenantManagementResource.java
@@ -13,13 +13,16 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.eclipse.hawkbit.mgmt.json.model.system.MgmtSystemTenantConfigurationValue;
 import org.eclipse.hawkbit.mgmt.json.model.system.MgmtSystemTenantConfigurationValueRequest;
 import org.eclipse.hawkbit.mgmt.rest.api.MgmtTenantManagementRestApi;
+import org.eclipse.hawkbit.repository.SystemManagement;
 import org.eclipse.hawkbit.repository.TenantConfigurationManagement;
 import org.eclipse.hawkbit.repository.model.TenantConfigurationValue;
 import org.eclipse.hawkbit.tenancy.configuration.TenantConfigurationProperties;
+import org.eclipse.hawkbit.tenancy.configuration.validator.TenantConfigurationValidatorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -39,20 +42,59 @@ public class MgmtTenantManagementResource implements MgmtTenantManagementRestApi
     private final TenantConfigurationManagement tenantConfigurationManagement;
     private final TenantConfigurationProperties tenantConfigurationProperties;
 
+    /**
+     * Default {@link org.eclipse.hawkbit.repository.model.DistributionSetType} as part of supported configs.
+     * */
+
+    private final SystemManagement systemManagement;
+
     MgmtTenantManagementResource(final TenantConfigurationManagement tenantConfigurationManagement,
-            final TenantConfigurationProperties tenantConfigurationProperties) {
+            final TenantConfigurationProperties tenantConfigurationProperties,
+        final SystemManagement systemManagement) {
         this.tenantConfigurationManagement = tenantConfigurationManagement;
         this.tenantConfigurationProperties = tenantConfigurationProperties;
+        this.systemManagement = systemManagement;
     }
 
     @Override
     public ResponseEntity<Map<String, MgmtSystemTenantConfigurationValue>> getTenantConfiguration() {
-        return ResponseEntity.ok(
-                MgmtTenantManagementMapper.toResponse(tenantConfigurationManagement, tenantConfigurationProperties));
+        //Load and Construct default Tenant Configuration
+        Map<String, MgmtSystemTenantConfigurationValue> tenantConfigurationValueMap = tenantConfigurationProperties.getConfigurationKeys().stream().collect(
+            Collectors.toMap(TenantConfigurationProperties.TenantConfigurationKey::getKeyName,
+            key -> loadTenantConfigurationValueBy(key.getKeyName())));
+        //Load and Add Default DistributionSetType
+        MgmtSystemTenantConfigurationValue defaultDsTypeId = loadTenantConfigurationValueBy(MgmtTenantManagementMapper.DEFAULT_DISTRIBUTION_SET_TYPE_KEY);
+        tenantConfigurationValueMap.put(MgmtTenantManagementMapper.DEFAULT_DISTRIBUTION_SET_TYPE_KEY, defaultDsTypeId);
+        //return combined TenantConfiguration and TenantMetadata
+        LOG.debug("getTenantConfiguration, return status {}", HttpStatus.OK);
+        return ResponseEntity.ok(tenantConfigurationValueMap);
+    }
+
+    @Override
+    public ResponseEntity<MgmtSystemTenantConfigurationValue> getTenantConfigurationValue(
+        @PathVariable("keyName") final String keyName) {
+        return ResponseEntity.ok(loadTenantConfigurationValueBy(keyName));
+    }
+
+    private MgmtSystemTenantConfigurationValue loadTenantConfigurationValueBy(String keyName) {
+
+        //Check if requested key is TenantConfiguration or TenantMetadata, load it and return it as rest response
+        MgmtSystemTenantConfigurationValue response;
+        if (isDefaultDistributionSetTypeKey(keyName)) {
+            response = MgmtTenantManagementMapper.toResponseDefaultDsType(String.valueOf(systemManagement.getTenantMetadata().getDefaultDsType().getId()));
+        } else {
+            response = MgmtTenantManagementMapper.toResponseTenantConfigurationValue(keyName, tenantConfigurationManagement.getConfigurationValue(keyName));
+        }
+        return response;
     }
 
     @Override
     public ResponseEntity<Void> deleteTenantConfigurationValue(@PathVariable("keyName") final String keyName) {
+
+        //Default DistributionSet Type cannot be deleted as is part of TenantMetadata
+        if (isDefaultDistributionSetTypeKey(keyName)) {
+            return ResponseEntity.badRequest().build();
+        }
 
         tenantConfigurationManagement.deleteConfiguration(keyName);
 
@@ -61,22 +103,20 @@ public class MgmtTenantManagementResource implements MgmtTenantManagementRestApi
     }
 
     @Override
-    public ResponseEntity<MgmtSystemTenantConfigurationValue> getTenantConfigurationValue(
-            @PathVariable("keyName") final String keyName) {
-
-        LOG.debug("{} config value getted, return status {}", keyName, HttpStatus.OK);
-        return ResponseEntity.ok(MgmtTenantManagementMapper.toResponse(keyName,
-                tenantConfigurationManagement.getConfigurationValue(keyName)));
-    }
-
-    @Override
     public ResponseEntity<MgmtSystemTenantConfigurationValue> updateTenantConfigurationValue(
             @PathVariable("keyName") final String keyName,
             @RequestBody final MgmtSystemTenantConfigurationValueRequest configurationValueRest) {
-
-        final TenantConfigurationValue<? extends Serializable> updatedValue = tenantConfigurationManagement
+        Serializable                  configurationValue            = configurationValueRest.getValue();
+        final MgmtSystemTenantConfigurationValue responseUpdatedValue;
+        if (isDefaultDistributionSetTypeKey(keyName)) {
+            responseUpdatedValue = updateDefaultDsType(configurationValue);
+        } else {
+            final TenantConfigurationValue<? extends Serializable> updatedTenantConfigurationValue = tenantConfigurationManagement
                 .addOrUpdateConfiguration(keyName, configurationValueRest.getValue());
-        return ResponseEntity.ok(MgmtTenantManagementMapper.toResponse(keyName, updatedValue));
+            responseUpdatedValue = MgmtTenantManagementMapper.toResponseTenantConfigurationValue(keyName, updatedTenantConfigurationValue);
+        }
+
+        return ResponseEntity.ok(responseUpdatedValue);
     }
 
     @Override
@@ -90,11 +130,54 @@ public class MgmtTenantManagementResource implements MgmtTenantManagementRestApi
             return ResponseEntity.badRequest().build();
         }
 
-        Map<String, TenantConfigurationValue<Serializable>> tenantConfigurationValues = tenantConfigurationManagement
-                .addOrUpdateConfiguration(configurationValueMap);
+        //Try update TenantMetadata first
+        Serializable defaultDsTypeValueUpdate = configurationValueMap.remove(MgmtTenantManagementMapper.DEFAULT_DISTRIBUTION_SET_TYPE_KEY);
+        Long oldDefaultDsType = null;
+        MgmtSystemTenantConfigurationValue updatedDefaultDsType = null;
+        if (defaultDsTypeValueUpdate != null) {
+            oldDefaultDsType = systemManagement.getTenantMetadata().getDefaultDsType().getId();
+            updatedDefaultDsType = updateDefaultDsType(defaultDsTypeValueUpdate);
+        }
+        //try update TenantConfiguration, in case of Error -> rollback TenantMetadata
+        Map<String, TenantConfigurationValue<Serializable>> tenantConfigurationValues;
+        try {
+            tenantConfigurationValues = tenantConfigurationManagement.addOrUpdateConfiguration(configurationValueMap);
+        } catch (Exception ex) {
+            //if DefaultDsType was updated, rollback it in case of TenantConfiguration update.
+            if (updatedDefaultDsType != null) {
+                systemManagement.updateTenantMetadata(oldDefaultDsType);
+            }
+            throw ex;
+        }
 
-        return ResponseEntity.ok(tenantConfigurationValues.entrySet().stream().map(entry ->
-                MgmtTenantManagementMapper.toResponse(entry.getKey(), entry.getValue())).toList());
+        List<MgmtSystemTenantConfigurationValue> tenantConfigurationListUpdated = new java.util.ArrayList<>(tenantConfigurationValues.entrySet().stream()
+            .map(entry -> MgmtTenantManagementMapper.toResponseTenantConfigurationValue(entry.getKey(), entry.getValue())).toList());
+        if (updatedDefaultDsType != null) {
+            tenantConfigurationListUpdated.add(updatedDefaultDsType);
+        }
+
+
+        return ResponseEntity.ok(tenantConfigurationListUpdated);
     }
+    private MgmtSystemTenantConfigurationValue updateDefaultDsType(Serializable defaultDsType) {
+        if (!defaultDsType.getClass().isAssignableFrom(String.class)) {
+            throw new TenantConfigurationValidatorException(String.format(
+                "Default DistributionSetType Value Type is incorrect. Expected Long, received %s", defaultDsType.getClass().getName()));
+        }
+        long defaultDistributionSetTypeValue;
+        try {
+            defaultDistributionSetTypeValue = Long.parseLong((String)defaultDsType);
+        } catch (Exception ex) {
+            throw new TenantConfigurationValidatorException(String.format(
+                "Default DistributionSetType Value Type is incorrect. Expected input to be a Number, received %s", defaultDsType.getClass().getName()));
+        }
+        systemManagement.updateTenantMetadata(defaultDistributionSetTypeValue);
+        return MgmtTenantManagementMapper.toResponseDefaultDsType((String)defaultDsType);
+    }
+
+    private static boolean isDefaultDistributionSetTypeKey(String keyName) {
+        return MgmtTenantManagementMapper.DEFAULT_DISTRIBUTION_SET_TYPE_KEY.equals(keyName);
+    }
+
 
 }
--- hawkbit-rest/hawkbit-mgmt-resource/src/test/java/org/eclipse/hawkbit/mgmt/rest/resource/MgmtTenantManagementResourceTest.java
+++ hawkbit-rest/hawkbit-mgmt-resource/src/test/java/org/eclipse/hawkbit/mgmt/rest/resource/MgmtTenantManagementResourceTest.java
@@ -9,14 +9,18 @@
  */
 package org.eclipse.hawkbit.mgmt.rest.resource;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.hawkbit.mgmt.json.model.system.MgmtSystemTenantConfigurationValueRequest;
 import org.eclipse.hawkbit.mgmt.rest.api.MgmtRestConstants;
+import org.eclipse.hawkbit.repository.model.DistributionSetType;
 import org.eclipse.hawkbit.rest.util.MockMvcResultPrinter;
 import org.eclipse.hawkbit.tenancy.configuration.TenantConfigurationProperties;
 import org.json.JSONObject;
@@ -43,22 +47,39 @@ public class MgmtTenantManagementResourceTest extends AbstractManagementApiInteg
     private static final String AUTHENTICATION_GATEWAYTOKEN_ENABLED = "authentication.gatewaytoken.enabled";
 
     private static final String AUTHENTICATION_GATEWAYTOKEN_KEY = "authentication.gatewaytoken.key";
+    private static final String DEFAULT_DISTRIBUTION_SET_TYPE_KEY = "default.ds.type";
 
     @Test
     @Description("Handles GET request for receiving all tenant specific configurations.")
     public void getTenantConfigurations() throws Exception {
         mvc.perform(get(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs/"))
                 .andDo(MockMvcResultPrinter.print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                //check for TenantMetadata additional properties
+                .andExpect(jsonPath("$.['" + DEFAULT_DISTRIBUTION_SET_TYPE_KEY + "']").exists())
+                .andExpect(jsonPath("$.['" + DEFAULT_DISTRIBUTION_SET_TYPE_KEY + "'].value", equalTo(getActualDefaultDsType())));
+
     }
 
     @Test
     @Description("Handles GET request for receiving a tenant specific configuration.")
     public void getTenantConfiguration() throws Exception {
+        //Test TenantConfiguration property
         mvc.perform(get(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs/{keyName}/",
                         TenantConfigurationProperties.TenantConfigurationKey.AUTHENTICATION_MODE_GATEWAY_SECURITY_TOKEN_KEY))
                 .andDo(MockMvcResultPrinter.print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @Description("Handles GET request for receiving (TenantMetadata - DefaultDsType) a tenant specific configuration.")
+    public void getTenantMetadata() throws Exception {
+        //Test TenantMetadata property
+        mvc.perform(get(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs/{keyName}/",
+                DEFAULT_DISTRIBUTION_SET_TYPE_KEY))
+            .andDo(MockMvcResultPrinter.print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.value", equalTo(getActualDefaultDsType())));
     }
 
     @Test
@@ -77,6 +98,57 @@ public class MgmtTenantManagementResourceTest extends AbstractManagementApiInteg
     }
 
     @Test
+    @Description("Handles PUT request for settings values (TenantMetadata - DefaultDsType) in tenant specific configuration, which is TenantMetadata")
+    public void putTenantMetadata() throws Exception {
+        final MgmtSystemTenantConfigurationValueRequest bodyPut = new MgmtSystemTenantConfigurationValueRequest();
+
+        String updatedTestDefaultDsType = createTestDistributionSetType();
+        bodyPut.setValue(updatedTestDefaultDsType);
+
+        final ObjectMapper mapper = new ObjectMapper();
+        final String json = mapper.writeValueAsString(bodyPut);
+
+        mvc.perform(put(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs/{keyName}/",
+                DEFAULT_DISTRIBUTION_SET_TYPE_KEY).content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(MockMvcResultPrinter.print())
+            .andExpect(status().isOk());
+
+        //check if after Rest success, value is really changed in TenantMetadata
+        assertEquals(updatedTestDefaultDsType, getActualDefaultDsType(), "Rest endpoint for updating the Default DistributionSetType completed successfully, but the actual value was not changed.");
+    }
+
+    private String createTestDistributionSetType() {
+        DistributionSetType testDefaultDsType = distributionSetTypeManagement.create(entityFactory.distributionSetType().create()
+            .key("test123").name("TestName123").description("TestDefaultDsType"));
+        testDefaultDsType = distributionSetTypeManagement
+            .update(entityFactory.distributionSetType().update(testDefaultDsType.getId()).description("TestDefaultDsType"));
+        return String.valueOf(testDefaultDsType.getId());
+    }
+
+    @Test
+    @Description("Update DefaultDistributionSetType Fails if given DistributionSetType ID does not exist.")
+    public void putTenantMetadataFails() throws Exception{
+        String oldDefaultDsType = getActualDefaultDsType();
+        //try an invalid input
+        String newDefaultDsType = new JSONObject().put("value", true).toString();
+        assertDefaultDsTypeUpdateFails(newDefaultDsType, oldDefaultDsType);
+        //try and invalid input
+        newDefaultDsType = new JSONObject().put("value", "someInvalidInput").toString();
+        assertDefaultDsTypeUpdateFails(newDefaultDsType, oldDefaultDsType);
+    }
+
+    private void assertDefaultDsTypeUpdateFails(String newDefaultDsType, String oldDefaultDsType) throws Exception {
+
+        mvc.perform(put(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs/{keyName}/",
+                DEFAULT_DISTRIBUTION_SET_TYPE_KEY).content(newDefaultDsType)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(MockMvcResultPrinter.print())
+            .andExpect(status().isBadRequest());
+        assertEquals(oldDefaultDsType, getActualDefaultDsType(), "Rest endpoint for updating DefaultDistributionType failed, but actual value changed unexpectedly.");
+    }
+
+    @Test
     @Description("The 'multi.assignments.enabled' property must not be changed to false.")
     public void deactivateMultiAssignment() throws Exception {
         final String bodyActivate = new JSONObject().put("value", true).toString();
@@ -92,33 +164,89 @@ public class MgmtTenantManagementResourceTest extends AbstractManagementApiInteg
     }
 
     @Test
-    @Description("The Batch configuration should be applied")
-    public void changeBatchConfiguration() throws Exception {
-        JSONObject configuration = new JSONObject();
-        configuration.put(ROLLOUT_APPROVAL_ENABLED, true);
-        configuration.put(AUTHENTICATION_GATEWAYTOKEN_ENABLED, true);
-        configuration.put(AUTHENTICATION_GATEWAYTOKEN_KEY, "1234");
-
-        String body = configuration.toString();
-
-        mvc.perform(put(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs")
-                        .content(body).contentType(MediaType.APPLICATION_JSON)).andDo(MockMvcResultPrinter.print())
-                .andExpect(status().isOk());
+    @Description("The Batch configuration should not be applied, because of invalid TenantConfiguration props")
+    public void changeBatchConfigurationFail() throws Exception {
+        //in this scenario
+        //  some TenantConfiguration are not valid,
+        //  TenantMetadata - DefaultDSType ID is valid,
+        //in the end batch configuration update must fail, and thus, not a single config should be actually changed
+        String     testValidDistributionSetType = createTestDistributionSetType();
+        boolean oldRolloutApprovalConfig = (Boolean) tenantConfigurationManagement.getConfigurationValue(ROLLOUT_APPROVAL_ENABLED).getValue();
+        String oldAuthGatewayToken = (String) tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_KEY).getValue();
+        //test TenantConfiguration with invalid config value, and a valid TenantMetadata - Default DistributionSetType id
+        assertBatchConfigurationFails(!oldRolloutApprovalConfig, "invalid-config-value", oldAuthGatewayToken + "randomSuffix0", testValidDistributionSetType);
     }
 
     @Test
-    @Description("The Batch configuration should not be applied")
-    public void changeBatchConfigurationFail() throws Exception {
+    @Description("The Batch configuration should not be applied, because of invalid TenantMetadata (DefaultDistributionSetType)")
+    public void changeBatchConfigurationFail2() throws Exception {
+        //in this scenario
+        //  all TenantConfiguration have valid and new values - using old values, inverted
+        //  TenantMetadata - DefaultDSType ID is invalid
+        //in the end batch configuration update must fail, and thus, not a single config should be actually changed.
+        boolean oldRolloutApprovalConfig = (Boolean) tenantConfigurationManagement.getConfigurationValue(ROLLOUT_APPROVAL_ENABLED).getValue();
+        boolean oldAuthGatewayTokenEnabled = (Boolean) tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_ENABLED).getValue();
+        String oldAuthGatewayToken = (String) tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_KEY).getValue();
+
+        //invalid TenantMetadata Default DistributionSetType, it is expected to be a number wrapped as String. Testing just any random String
+        //not a single configuration should be changed after the failure
+        Object     testInvalidDistributionSetType = "someInvalidInput";
+        assertBatchConfigurationFails(!oldRolloutApprovalConfig, !oldAuthGatewayTokenEnabled, oldAuthGatewayToken + "randomSuffix1", testInvalidDistributionSetType);
+
+        //invalid TenantMetadata Default DistributionSetType, it is expected to be a number wrapped as String. Testing not a String at all
+        //not a single configuration should be changed after the failure
+        testInvalidDistributionSetType = true;
+        assertBatchConfigurationFails(!oldRolloutApprovalConfig, !oldAuthGatewayTokenEnabled, oldAuthGatewayToken + "randomSuffix2", testInvalidDistributionSetType);
+    }
+
+    private void assertBatchConfigurationFails(Object newRolloutApprovalEnabled, Object newAuthGatewayTokenEnabled, Object newGatewayToken, Object newDistributionSetTypeId) throws Exception {
+        String oldDefaultDsType = getActualDefaultDsType();
+        boolean oldRolloutApprovalConfig = (Boolean) tenantConfigurationManagement.getConfigurationValue(ROLLOUT_APPROVAL_ENABLED).getValue();
+        boolean oldAuthGatewayTokenEnabled = (Boolean) tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_ENABLED).getValue();
+        String oldAuthGatewayToken = (String) tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_KEY).getValue();
+
         JSONObject configuration = new JSONObject();
-        configuration.put(ROLLOUT_APPROVAL_ENABLED, true);
-        configuration.put(AUTHENTICATION_GATEWAYTOKEN_ENABLED, "wrong");
-        configuration.put(AUTHENTICATION_GATEWAYTOKEN_KEY, "1234");
+        //if new values are not passed.. use old values inverted.. or append random Strings
+        configuration.put(ROLLOUT_APPROVAL_ENABLED, newRolloutApprovalEnabled);
+        configuration.put(AUTHENTICATION_GATEWAYTOKEN_ENABLED, newAuthGatewayTokenEnabled);
+        configuration.put(AUTHENTICATION_GATEWAYTOKEN_KEY, newGatewayToken);
+        configuration.put(DEFAULT_DISTRIBUTION_SET_TYPE_KEY, newDistributionSetTypeId);
+        String body = configuration.toString();
+
+        mvc.perform(put(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs")
+                .content(body).contentType(MediaType.APPLICATION_JSON)).andDo(MockMvcResultPrinter.print())
+            .andExpect(status().isBadRequest());
+        //Check if TenantMetadata and TenantConfiguration is not changed as Batch config failed
+        assertEquals(oldDefaultDsType, getActualDefaultDsType(), "Batch configuration update Failed, but TenantMetadata - DistributionSetType was actually changed.");
+        assertEquals(oldRolloutApprovalConfig, tenantConfigurationManagement.getConfigurationValue(ROLLOUT_APPROVAL_ENABLED).getValue(), "Batch configuration update Failed, but TenantConfiguration was actually changed.");
+        assertEquals(oldAuthGatewayTokenEnabled, tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_ENABLED).getValue(), "Batch configuration update Failed, but TenantConfiguration was actually changed.");
+        assertEquals(oldAuthGatewayToken, tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_KEY).getValue(), "Batch configuration update Failed, but TenantConfiguration was actually changed.");
+    }
+
+    @Test
+    @Description("The Batch configuration should be applied")
+    public void changeBatchConfiguration() throws Exception {
+        String     updatedDistributionSetType = createTestDistributionSetType();
+        boolean updatedRolloutApprovalEnabled = true;
+        boolean updatedAuthGatewayTokenEnabled = true;
+        String updatedAuthGatewayTokenKey = "54321";
+        JSONObject configuration = new JSONObject();
+        configuration.put(ROLLOUT_APPROVAL_ENABLED, updatedRolloutApprovalEnabled);
+        configuration.put(AUTHENTICATION_GATEWAYTOKEN_ENABLED, updatedAuthGatewayTokenEnabled);
+        configuration.put(AUTHENTICATION_GATEWAYTOKEN_KEY, updatedAuthGatewayTokenKey);
+        configuration.put(DEFAULT_DISTRIBUTION_SET_TYPE_KEY, updatedDistributionSetType);
 
         String body = configuration.toString();
 
         mvc.perform(put(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs")
-                        .content(body).contentType(MediaType.APPLICATION_JSON)).andDo(MockMvcResultPrinter.print())
-                .andExpect(status().isBadRequest());
+                .content(body).contentType(MediaType.APPLICATION_JSON)).andDo(MockMvcResultPrinter.print())
+            .andExpect(status().isOk());
+
+        //assert all changes were applied after Rest Success
+        assertEquals(updatedDistributionSetType, getActualDefaultDsType(), "Change BatchConfiguration was successful but TenantMetadata - Default DistributionSetType was not actually changed.");
+        assertEquals(updatedRolloutApprovalEnabled, tenantConfigurationManagement.getConfigurationValue(ROLLOUT_APPROVAL_ENABLED).getValue(), "Change BatchConfiguration was successful but TenantConfiguration property was not actually changed.");
+        assertEquals(updatedAuthGatewayTokenEnabled, tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_ENABLED).getValue(), "Change BatchConfiguration was successful but TenantConfiguration property was not actually changed.");
+        assertEquals(updatedAuthGatewayTokenKey, tenantConfigurationManagement.getConfigurationValue(AUTHENTICATION_GATEWAYTOKEN_KEY).getValue(), "Change BatchConfiguration was successful but TenantConfiguration property was not actually changed.");
     }
 
     @Test
@@ -150,5 +278,18 @@ public class MgmtTenantManagementResourceTest extends AbstractManagementApiInteg
                         TenantConfigurationProperties.TenantConfigurationKey.AUTHENTICATION_MODE_GATEWAY_SECURITY_TOKEN_KEY))
                 .andDo(MockMvcResultPrinter.print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @Description("Tests DELETE request must Fail for TenantMetadata properties.")
+    public void deleteTenantMetadataFail() throws Exception {
+        mvc.perform(delete(MgmtRestConstants.SYSTEM_V1_REQUEST_MAPPING + "/configs/{keyName}/",
+                DEFAULT_DISTRIBUTION_SET_TYPE_KEY))
+            .andDo(MockMvcResultPrinter.print())
+            .andExpect(status().isBadRequest());
+    }
+
+    private String getActualDefaultDsType() {
+        return String.valueOf(systemManagement.getTenantMetadata().getDefaultDsType().getId());
     }
 }
