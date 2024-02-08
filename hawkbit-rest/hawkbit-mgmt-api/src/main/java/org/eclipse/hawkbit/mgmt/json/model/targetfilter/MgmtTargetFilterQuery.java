3f6af46b0f3ece3d106557a7fd105665e89cee2d
Marinov Avgustin <Avgustin.Marinov@bosch.io>
2024-02-08 15:22:59 +0200

    REST doc / Mgmt Target Filters - fix missed info
    When spring restdoc was replaces with swagger & open api some info was lost
    This commit returns back this info for Mgmt API - TargetFilters
    
    Signed-off-by: Marinov Avgustin <Avgustin.Marinov@bosch.com>

--- hawkbit-rest/hawkbit-mgmt-api/src/main/java/org/eclipse/hawkbit/mgmt/json/model/targetfilter/MgmtTargetFilterQuery.java
+++ hawkbit-rest/hawkbit-mgmt-api/src/main/java/org/eclipse/hawkbit/mgmt/json/model/targetfilter/MgmtTargetFilterQuery.java
@@ -27,52 +27,59 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @EqualsAndHashCode(callSuper = true)
 @JsonInclude(Include.ALWAYS)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Schema(example = """
+@Schema(description = """
+    **_links**:
+    * **autoAssignDS** - Link to manage the auto assign distribution set
+    """, example = """
     {
-      "createdBy" : "bumlux",
-      "createdAt" : 1682408575234,
-      "lastModifiedBy" : "bumlux",
-      "lastModifiedAt" : 1682408575234,
-      "name" : "filter1",
-      "query" : "name==a",
-      "autoAssignDistributionSet" : 16,
-      "autoAssignActionType" : null,
-      "autoAssignWeight" : null,
-      "confirmationRequired" : null,
-      "_links" : {
-        "self" : {
-          "href" : "https://management-api.host.com/rest/v1/targetfilters/2"
-        }
-      },
-      "id" : 2
-    }""")
+       "createdBy" : "bumlux",
+       "createdAt" : 1682408566380,
+       "lastModifiedBy" : "bumlux",
+       "lastModifiedAt" : 1682408566385,
+       "name" : "filter1",
+       "query" : "name==*",
+       "autoAssignDistributionSet" : 3,
+       "autoAssignActionType" : "forced",
+       "autoAssignWeight" : null,
+       "confirmationRequired" : null,
+       "_links" : {
+         "self" : {
+           "href" : "https://management-api.host.com/rest/v1/targetfilters/5"
+         },
+         "autoAssignDS" : {
+           "href" : "https://management-api.host.com/rest/v1/targetfilters/5/autoAssignDS"
+         }
+       },
+       "id" : 5
+     }""")
 public class MgmtTargetFilterQuery extends MgmtBaseEntity {
 
     @JsonProperty(value = "id", required = true)
-    @Schema(example = "2")
+    @Schema(description = "The technical identifier of the entity", example = "2")
     private Long filterId;
 
     @JsonProperty
-    @Schema(example = "filterName")
+    @Schema(description = "The name of the entity", example = "filterName")
     private String name;
 
     @JsonProperty
-    @Schema(example = "name==*")
+    @Schema(description = "Target filter query expression", example = "name==*")
     private String query;
 
     @JsonProperty
     @Schema(example = "15")
-
     private Long autoAssignDistributionSet;
 
     @JsonProperty
+    @Schema(description = "Auto assign distribution set id")
     private MgmtActionType autoAssignActionType;
 
     @JsonProperty
-    @Schema(example = "")
+    @Schema(description = "Weight of the resulting Actions", example = "600")
     private Integer autoAssignWeight;
 
     @JsonProperty
-    @Schema(example = "false")
+    @Schema(description = "(Available with user consent flow active) Defines, if the confirmation is required for an " +
+            "action. Confirmation is required per default.", example = "false")
     private Boolean confirmationRequired;
 }
