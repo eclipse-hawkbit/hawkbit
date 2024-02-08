f5359d6bc9063253841cc0284ca433646ca08a63
Avgustin Marinov <Avgustin.Marinov@bosch.com>
2024-02-08 14:55:06 +0200

    REST doc / Mgmt Software Modules - fix missed info (#1625)
    When spring restdoc was replaces with swagger & open api some info was lost
    This commit returns back this info for Mgmt API - SoftwareModules
    
    Signed-off-by: Marinov Avgustin <Avgustin.Marinov@bosch.com>

--- hawkbit-rest/hawkbit-mgmt-api/src/main/java/org/eclipse/hawkbit/mgmt/json/model/softwaremodule/MgmtSoftwareModule.java
+++ hawkbit-rest/hawkbit-mgmt-api/src/main/java/org/eclipse/hawkbit/mgmt/json/model/softwaremodule/MgmtSoftwareModule.java
@@ -26,25 +26,66 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @EqualsAndHashCode(callSuper = true)
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(description = """
+    **_links**:
+    * **type** - The software module type of the entity
+    * **artifacts** - List of artifacts of given software module
+    * **metadata** - List of metadata
+    """, example = """
+    {
+      "createdBy" : "bumlux",
+      "createdAt" : 1682408572790,
+      "lastModifiedBy" : "bumlux",
+      "lastModifiedAt" : 1682408572791,
+      "name" : "os",
+      "description" : "a description",
+      "version" : "1.0",
+      "type" : "os",
+      "typeName" : "OS",
+      "vendor" : "Vendor Limited, California",
+      "deleted" : false,
+      "encrypted" : false,
+      "_links" : {
+        "self" : {
+          "href" : "https://management-api.host.com/rest/v1/softwaremodules/6"
+        },
+        "artifacts" : {
+          "href" : "https://management-api.host.com/rest/v1/softwaremodules/6/artifacts"
+        },
+        "type" : {
+          "href" : "https://management-api.host.com/rest/v1/softwaremoduletypes/13"
+        },
+        "metadata" : {
+          "href" : "https://management-api.host.com/rest/v1/softwaremodules/6/metadata?offset=0&limit=50"
+        }
+      },
+      "id" : 6
+    }""")
 public class MgmtSoftwareModule extends MgmtNamedEntity {
 
     @JsonProperty(value = "id", required = true)
     @Schema(description = "The technical identifier of the entity", example = "6")
     private Long moduleId;
+
     @JsonProperty(required = true)
     @Schema(description = "Package version", example = "1.0.0")
     private String version;
+
     @JsonProperty(required = true)
     @Schema(description = "The software module type of the entity", example = "os")
     private String type;
+
     @Schema(description = "The software module type name of the entity", example = "OS")
     private String typeName;
+
     @JsonProperty
     @Schema(description = "The software vendor", example = "Vendor Limited, California")
     private String vendor;
+
     @JsonProperty
     @Schema(description = "If the software module is deleted", example = "false")
     private boolean deleted;
+
     @JsonProperty
     @Schema(description = "If the software module is encrypted", example = "false")
     private boolean encrypted;
