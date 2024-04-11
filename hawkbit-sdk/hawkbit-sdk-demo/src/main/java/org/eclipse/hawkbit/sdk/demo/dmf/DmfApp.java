3497d155a10ccc83621c6e93667d9411e6adca07
Avgustin Marinov <Avgustin.Marinov@bosch.com>
2024-04-11 17:43:33 +0300

    DmfApp license header fixed (#1711)
    Signed-off-by: Marinov Avgustin <Avgustin.Marinov@bosch.com>

--- hawkbit-sdk/hawkbit-sdk-demo/src/main/java/org/eclipse/hawkbit/sdk/demo/dmf/DmfApp.java
+++ hawkbit-sdk/hawkbit-sdk-demo/src/main/java/org/eclipse/hawkbit/sdk/demo/dmf/DmfApp.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024 Bosch.IO GmbH and others
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
