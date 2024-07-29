6106d3c16c3f6e5549a73616a5afcfeceb50f249
Avgustin Marinov <Avgustin.Marinov@bosch.com>
2024-07-29 13:50:42 +0300

    Fix sonar findings (#1792)
    Signed-off-by: Marinov Avgustin <Avgustin.Marinov@bosch.com>

--- hawkbit-security-core/src/main/java/org/eclipse/hawkbit/im/authentication/StaticAuthenticationProvider.java
+++ hawkbit-security-core/src/main/java/org/eclipse/hawkbit/im/authentication/StaticAuthenticationProvider.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
