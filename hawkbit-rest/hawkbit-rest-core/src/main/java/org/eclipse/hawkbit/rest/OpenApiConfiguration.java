664c1bd8350de2f1b4b52a538b0df5f584132c79
Avgustin Marinov <Avgustin.Marinov@bosch.com>
2024-11-06 11:57:15 +0200

    Split open api config to DDI and MGMT resources
    Signed-off-by: Avgustin Marinov <Avgustin.Marinov@bosch.com>

--- hawkbit-rest/hawkbit-rest-core/src/main/java/org/eclipse/hawkbit/rest/OpenApiConfiguration.java
+++ hawkbit-rest/hawkbit-rest-core/src/main/java/org/eclipse/hawkbit/rest/OpenApiConfiguration.java
@@ -11,93 +11,27 @@ package org.eclipse.hawkbit.rest;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnProperty(
-        value = "hawkbit.server.swagger.enabled",
+        value = OpenApiConfiguration.HAWKBIT_SERVER_SWAGGER_ENABLED,
         havingValue = "true",
         matchIfMissing = true)
 public class OpenApiConfiguration {
+
+    public static final String HAWKBIT_SERVER_SWAGGER_ENABLED = "hawkbit.server.swagger.enabled";
 
     private static final String API_TITLE = "hawkBit REST APIs";
     private static final String API_VERSION = "v1";
     private static final String DESCRIPTION = """
             Eclipse hawkBit™ is a domain-independent back-end framework for rolling out software updates to constrained edge devices as well as more powerful controllers and gateways connected to IP based networking infrastructure.
-              """;
-
-    private static final String BASIC_AUTH_SEC_SCHEME_NAME = "Basic Authentication";
-    private static final String BEARER_AUTH_SEC_SCHEME_NAME = "Bearer Authentication";
-    private static final String DDI_TOKEN_SEC_SCHEME_NAME = "DDI Target/GatewayToken Authentication";
+            """;
 
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI().info(new Info().title(API_TITLE).version(API_VERSION).description(DESCRIPTION));
-    }
-
-    @Bean
-    @ConditionalOnProperty(
-            value = "hawkbit.server.swagger.mgmt.api.group.enabled",
-            havingValue = "true",
-            matchIfMissing = true)
-    public GroupedOpenApi mgmtApi() {
-        return GroupedOpenApi
-                .builder()
-                .group("Management API")
-                .pathsToMatch("/rest/v1/**")
-                .addOpenApiCustomizer(openApi -> {
-                    openApi
-                            .addSecurityItem(new SecurityRequirement()
-                                    .addList(BASIC_AUTH_SEC_SCHEME_NAME)
-                                    .addList(BEARER_AUTH_SEC_SCHEME_NAME))
-                            .components(
-                                    openApi
-                                            .getComponents()
-                                            .addSecuritySchemes(BASIC_AUTH_SEC_SCHEME_NAME,
-                                                    new SecurityScheme()
-                                                            .name(BASIC_AUTH_SEC_SCHEME_NAME)
-                                                            .type(SecurityScheme.Type.HTTP)
-                                                            .in(SecurityScheme.In.HEADER)
-                                                            .scheme("basic"))
-                                            .addSecuritySchemes(BEARER_AUTH_SEC_SCHEME_NAME,
-                                                    new SecurityScheme()
-                                                            .name(BEARER_AUTH_SEC_SCHEME_NAME)
-                                                            .type(SecurityScheme.Type.HTTP)
-                                                            .in(SecurityScheme.In.HEADER)
-                                                            .bearerFormat("JWT")
-                                                            .scheme("bearer")));
-                })
-                .build();
-    }
-
-    @Bean
-    @ConditionalOnProperty(
-            value = "hawkbit.server.swagger.ddi.api.group.enabled",
-            havingValue = "true",
-            matchIfMissing = true)
-    public GroupedOpenApi ddiApi() {
-        return GroupedOpenApi
-                .builder()
-                .group("Direct Device Integration API")
-                .pathsToMatch("/{tenant}/controller/**")
-                .addOpenApiCustomizer(openApi -> {
-                    openApi
-                            .addSecurityItem(new SecurityRequirement().addList(DDI_TOKEN_SEC_SCHEME_NAME))
-                            .components(
-                                    openApi
-                                            .getComponents()
-                                            .addSecuritySchemes(DDI_TOKEN_SEC_SCHEME_NAME,
-                                                    new SecurityScheme()
-                                                            .name("Authorization")
-                                                            .type(SecurityScheme.Type.APIKEY)
-                                                            .in(SecurityScheme.In.HEADER)
-                                                            .description("Format: (Target|Gateway)Token &lt;token&gt;")));
-                })
-                .build();
     }
 }
