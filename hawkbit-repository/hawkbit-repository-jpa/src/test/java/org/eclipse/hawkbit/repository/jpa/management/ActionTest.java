0b6095b46dd767a93f23032a3eae607f8747d7b6
Avgustin Marinov <Avgustin.Marinov@bosch.com>
2024-11-15 17:10:20 +0200

    Switch JpaActionStatus to javax.persistence.Convert(er)
    Signed-off-by: Avgustin Marinov <Avgustin.Marinov@bosch.com>

--- hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/management/ActionTest.java
+++ hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/management/ActionTest.java
@@ -12,6 +12,7 @@ package org.eclipse.hawkbit.repository.jpa.management;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
+import java.util.List;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
@@ -22,15 +23,21 @@ import org.eclipse.hawkbit.repository.jpa.model.JpaAction;
 import org.eclipse.hawkbit.repository.model.Action;
 import org.eclipse.hawkbit.repository.model.Action.ActionType;
 import org.eclipse.hawkbit.repository.model.Action.Status;
+import org.eclipse.hawkbit.repository.model.ActionStatus;
+import org.eclipse.hawkbit.repository.model.DistributionSet;
+import org.eclipse.hawkbit.repository.model.Target;
 import org.junit.jupiter.api.Test;
 
 @Feature("Unit Tests - Repository")
 @Story("Deployment Management")
-public class ActionTest extends AbstractJpaIntegrationTest {
+class ActionTest extends AbstractJpaIntegrationTest {
+
+    private Target target;
+    private DistributionSet distributionSet;
 
     @Test
     @Description("Ensures that timeforced moded switch from soft to forces after defined timeframe.")
-    public void timeforcedHitNewHasCodeIsGenerated() {
+    void timeforcedHitNewHasCodeIsGenerated() {
 
         // current time + 1 seconds
         final long sleepTime = 1000;
@@ -46,7 +53,7 @@ public class ActionTest extends AbstractJpaIntegrationTest {
     }
 
     @Test
-    public void testActionTypeConvert() {
+    void testActionTypeConvert() {
         final long id = createAction().getId();
         for (final ActionType actionType : ActionType.values()) {
             final JpaAction action = actionRepository
@@ -59,7 +66,7 @@ public class ActionTest extends AbstractJpaIntegrationTest {
     }
 
     @Test
-    public void testStatusConvert() {
+    void testStatusConvert() {
         final long id = createAction().getId();
         for (final Status status : Status.values()) {
             final JpaAction action = actionRepository
@@ -71,15 +78,36 @@ public class ActionTest extends AbstractJpaIntegrationTest {
         }
     }
 
+    @Test
+    void testActionsStatusStatusConvert() {
+        for (final Status status : Status.values()) {
+            final long id = createAction().getId();
+            controllerManagement.addUpdateActionStatus(entityFactory.actionStatus().create(id).status(status));
+            final List<ActionStatus> actionStatuses = actionRepository
+                    .findById(id).orElseThrow(() -> new IllegalStateException("Action not found"))
+                    .getActionStatus();
+            assertThat(actionStatuses).hasSize(1);
+            assertThat(actionStatuses.stream().filter(as -> as.getStatus() == status).count()).isEqualTo(1);
+        }
+    }
+
     private Action createAction() {
+        if (target == null) {
+            target = testdataFactory.createTarget("testActionTypeMappingTarget");
+        }
+        if (distributionSet == null) {
+            distributionSet = testdataFactory.createDistributionSet("testActionTypeMappingDS");
+        }
+
         final ActionType[] actionTypes = ActionType.values();
         final JpaAction action = new JpaAction();
-        action.setTarget(testdataFactory.createTarget("testActionTypeMappingTarget"));
-        action.setDistributionSet(testdataFactory.createDistributionSet("testActionTypeMappingDS"));
+        action.setTarget(target);
+        action.setDistributionSet(distributionSet);
         action.setActionType(actionTypes[0]);
         action.setStatus(Status.SCHEDULED);
         action.setWeight(100);
         action.setInitiatedBy("none");
+        action.setActive(true);
         return actionRepository.save(action);
     }
 }
