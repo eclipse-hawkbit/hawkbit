293cc938da945ba00c76ed7c40d0ff3ff1f6ebd5
Denislav Prinov <denislav.prinov@bosch.com>
2025-02-06 08:56:10 +0200

    License header fix
    Signed-off-by: Denislav Prinov <denislav.prinov@bosch.com>

--- hawkbit-repository/hawkbit-repository-jpa/src/main/java/org/eclipse/hawkbit/repository/jpa/rollout/BlockWhenFullPolicy.java
+++ hawkbit-repository/hawkbit-repository-jpa/src/main/java/org/eclipse/hawkbit/repository/jpa/rollout/BlockWhenFullPolicy.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) 2025 Contributors to the Eclipse Foundation
- * <p>
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * <p>
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 
