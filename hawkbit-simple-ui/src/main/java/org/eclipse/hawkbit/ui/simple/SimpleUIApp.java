424520bb7255b6ae6b798fac65153e94e0874807
Mohamed Zenadi <zeapo@users.noreply.github.com>
2025-05-20 08:59:41 +0200

    SimpleUI: add oauth2 support (#2400)
    * simple-ui: add oauth2 support
    
    This uses a trick to ensure that the id token is refreshed; it needs to be revisited after Spring Security 6.5 is released
    
    * fixed style and added a way to enable / disable oauth2 client

--- hawkbit-simple-ui/src/main/java/org/eclipse/hawkbit/ui/simple/SimpleUIApp.java
+++ hawkbit-simple-ui/src/main/java/org/eclipse/hawkbit/ui/simple/SimpleUIApp.java
@@ -9,13 +9,6 @@
  */
 package org.eclipse.hawkbit.ui.simple;
 
-import static feign.Util.ISO_8859_1;
-
-import java.util.Base64;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
-
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
@@ -28,7 +21,9 @@ import feign.codec.ErrorDecoder;
 import org.eclipse.hawkbit.sdk.HawkbitClient;
 import org.eclipse.hawkbit.sdk.HawkbitServer;
 import org.eclipse.hawkbit.sdk.Tenant;
+import org.eclipse.hawkbit.ui.simple.security.OAuth2TokenManager;
 import org.eclipse.hawkbit.ui.simple.view.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
@@ -40,6 +35,21 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import java.util.Base64;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+import static feign.Util.ISO_8859_1;
+import static java.util.Collections.emptyList;
 
 @Theme(themeClass = Lumo.class)
 @PWA(name = "hawkBit UI", shortName = "hawkBit UI")
@@ -47,11 +57,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @Import(FeignClientsConfiguration.class)
 public class SimpleUIApp implements AppShellConfigurator {
 
-    private static final RequestInterceptor AUTHORIZATION = requestTemplate -> {
+    private static final Function<OAuth2TokenManager, RequestInterceptor> AUTHORIZATION = (oAuth2TokenManager) -> requestTemplate -> {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        requestTemplate.header("Authorization", "Basic " + Base64.getEncoder().encodeToString(
-                (Objects.requireNonNull(authentication.getPrincipal(), "User is null!") + ":" + Objects.requireNonNull(
-                        authentication.getCredentials(), "Password is not available!")).getBytes(ISO_8859_1)));
+        if (oAuth2TokenManager != null && authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+            String bearerToken = oAuth2TokenManager.getToken(oAuth2AuthenticationToken);
+            requestTemplate.header("Authorization", "Bearer " + bearerToken);
+        } else {
+            requestTemplate.header(
+                    "Authorization", "Basic " + Base64.getEncoder().encodeToString(
+                            (Objects.requireNonNull(authentication.getPrincipal(), "User is null!") + ":" + Objects.requireNonNull(
+                                    authentication.getCredentials(), "Password is not available!")).getBytes(ISO_8859_1))
+            );
+        }
     };
 
     private static final ErrorDecoder DEFAULT_ERROR_DECODER = new ErrorDecoder.Default();
@@ -67,18 +84,47 @@ public class SimpleUIApp implements AppShellConfigurator {
 
     @Bean
     HawkbitClient hawkbitClient(
-            final HawkbitServer hawkBitServer, final Encoder encoder, final Decoder decoder, final Contract contract) {
+            final HawkbitServer hawkBitServer,
+            final Encoder encoder,
+            final Decoder decoder,
+            final Contract contract,
+            @Autowired(required = false)
+            final OAuth2TokenManager oAuth2TokenManager
+    ) {
         return new HawkbitClient(
                 hawkBitServer, encoder, decoder, contract,
                 ERROR_DECODER,
                 (tenant, controller) ->
-                        controller == null ?
-                                AUTHORIZATION : HawkbitClient.DEFAULT_REQUEST_INTERCEPTOR_FN.apply(tenant, controller));
+                        controller == null
+                                ? AUTHORIZATION.apply(oAuth2TokenManager)
+                                : HawkbitClient.DEFAULT_REQUEST_INTERCEPTOR_FN.apply(tenant, controller)
+        );
     }
 
     @Bean
     HawkbitMgmtClient hawkbitMgmtClient(final Tenant tenant, final HawkbitClient hawkbitClient) {
         return new HawkbitMgmtClient(tenant, hawkbitClient);
+    }
+
+    @Bean
+    OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService(final HawkbitMgmtClient hawkbitClient) {
+        final OidcUserService delegate = new OidcUserService();
+        return (userRequest) -> {
+            OidcUser oidcUser = delegate.loadUser(userRequest);
+
+            final OAuth2AuthenticationToken tempToken = new OAuth2AuthenticationToken(
+                    oidcUser,
+                    emptyList(),
+                    userRequest.getClientRegistration().getRegistrationId()
+            );
+            final List<SimpleGrantedAuthority> grantedAuthorities =
+                    getGrantedAuthorities(hawkbitClient, tempToken);
+            return new DefaultOidcUser(
+                    grantedAuthorities,
+                    oidcUser.getIdToken(),
+                    oidcUser.getUserInfo()
+            );
+        };
     }
 
     // accepts all user / pass, just delegating them to the feign client
@@ -88,35 +134,9 @@ public class SimpleUIApp implements AppShellConfigurator {
             final String username = authentication.getName();
             final String password = authentication.getCredentials().toString();
 
-            final List<String> roles = new LinkedList<>();
-            roles.add("ANONYMOUS");
-            final SecurityContext unauthorizedContext = SecurityContextHolder.createEmptyContext();
-            unauthorizedContext.setAuthentication(
-                    new UsernamePasswordAuthenticationToken(username, password));
-            final SecurityContext currentContext = SecurityContextHolder.getContext();
-            try {
-                SecurityContextHolder.setContext(unauthorizedContext);
-                if (hawkbitClient.hasSoftwareModulesRead()) {
-                    roles.add("SOFTWARE_MODULE_READ");
-                }
-                if (hawkbitClient.hasRolloutRead()) {
-                    roles.add("ROLLOUT_READ");
-                }
-                if (hawkbitClient.hasDistributionSetRead()) {
-                    roles.add("DISTRIBUTION_SET_READ");
-                }
-                if (hawkbitClient.hasTargetRead()) {
-                    roles.add("TARGET_READ");
-                }
-                if (hawkbitClient.hasConfigRead()) {
-                    roles.add("CONFIG_READ");
-                }
-            } finally {
-                SecurityContextHolder.setContext(currentContext);
-            }
-            return new UsernamePasswordAuthenticationToken(
-                    username, password,
-                    roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).toList()) {
+            final List<SimpleGrantedAuthority> grantedAuthorities = getGrantedAuthorities(
+                    hawkbitClient, new UsernamePasswordAuthenticationToken(username, password));
+            return new UsernamePasswordAuthenticationToken(username, password, grantedAuthorities) {
 
                 @Override
                 public void eraseCredentials() {
@@ -125,5 +145,34 @@ public class SimpleUIApp implements AppShellConfigurator {
                 }
             };
         };
+    }
+
+    private List<SimpleGrantedAuthority> getGrantedAuthorities(final HawkbitMgmtClient hawkbitClient, Authentication authentication) {
+        final List<String> roles = new LinkedList<>();
+        roles.add("ANONYMOUS");
+        final SecurityContext unauthorizedContext = SecurityContextHolder.createEmptyContext();
+        unauthorizedContext.setAuthentication(authentication);
+        final SecurityContext currentContext = SecurityContextHolder.getContext();
+        try {
+            SecurityContextHolder.setContext(unauthorizedContext);
+            if (hawkbitClient.hasSoftwareModulesRead()) {
+                roles.add("SOFTWARE_MODULE_READ");
+            }
+            if (hawkbitClient.hasRolloutRead()) {
+                roles.add("ROLLOUT_READ");
+            }
+            if (hawkbitClient.hasDistributionSetRead()) {
+                roles.add("DISTRIBUTION_SET_READ");
+            }
+            if (hawkbitClient.hasTargetRead()) {
+                roles.add("TARGET_READ");
+            }
+            if (hawkbitClient.hasConfigRead()) {
+                roles.add("CONFIG_READ");
+            }
+        } finally {
+            SecurityContextHolder.setContext(currentContext);
+        }
+        return roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).toList();
     }
 }
