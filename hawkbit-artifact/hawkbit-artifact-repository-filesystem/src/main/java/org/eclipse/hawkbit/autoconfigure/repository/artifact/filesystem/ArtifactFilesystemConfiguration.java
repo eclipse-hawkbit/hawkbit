e36a40955be8c72b6fc3663a484edb12b8fdd83c
Avgustin Marinov <Avgustin.Marinov@bosch.com>
2025-07-10 12:09:31 +0300

    Rename autoconfig for filesystem package
    Signed-off-by: Avgustin Marinov <Avgustin.Marinov@bosch.com>

--- hawkbit-artifact/hawkbit-artifact-repository-filesystem/src/main/java/org/eclipse/hawkbit/autoconfigure/repository/artifact/filesystem/ArtifactFilesystemConfiguration.java
+++ hawkbit-artifact/hawkbit-artifact-repository-filesystem/src/main/java/org/eclipse/hawkbit/autoconfigure/repository/artifact/filesystem/ArtifactFilesystemConfiguration.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015 Bosch Software Innovations GmbH and others
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.eclipse.hawkbit.autoconfigure.artifact.repository.filesystem;
+package org.eclipse.hawkbit.autoconfigure.repository.artifact.filesystem;
 
 import org.eclipse.hawkbit.repository.artifact.ArtifactFilesystemProperties;
 import org.eclipse.hawkbit.repository.artifact.ArtifactFilesystemRepository;
