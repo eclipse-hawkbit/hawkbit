aa674d12e4572330dfc4968ef21158cbd1647b62
Florian Bezannier <florian.bezannier@hotmail.fr>
2025-07-11 17:20:47 +0200

    fix[Simple-UI]: securityToken null if no permission

--- hawkbit-simple-ui/src/main/java/org/eclipse/hawkbit/ui/simple/view/TargetView.java
+++ hawkbit-simple-ui/src/main/java/org/eclipse/hawkbit/ui/simple/view/TargetView.java
@@ -405,7 +405,7 @@ public class TargetView extends TableView<TargetView.TargetWithDs, String> {
             createdAt.setValue(new Date(target.getCreatedAt()).toString());
             lastModifiedBy.setValue(target.getLastModifiedBy());
             lastModifiedAt.setValue(new Date(target.getLastModifiedAt()).toString());
-            securityToken.setValue(target.getSecurityToken());
+            securityToken.setValue(Objects.requireNonNullElse(target.getSecurityToken(), ""));
             group.setValue(target.getGroup() != null ? target.getGroup() : "");
             targetAddress.setValue(target.getAddress() != null ? target.getAddress() : "");
 
