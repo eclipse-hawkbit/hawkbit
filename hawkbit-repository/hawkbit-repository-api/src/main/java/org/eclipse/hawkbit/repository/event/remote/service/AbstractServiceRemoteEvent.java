a55d5cd5176b8b941968f50d09a794e82166c9db
Vasil Ilchev <vasil.ilchev@bosch.io>
2025-08-06 09:46:19 +0300

    Fix class cast exception when deserielize getRemoteEvent() by any ServiceEvent (#2593)
    Co-authored-by: vasilchev <vasil.ilchev@bosch.com>

--- hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/event/remote/service/AbstractServiceRemoteEvent.java
+++ hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/event/remote/service/AbstractServiceRemoteEvent.java
@@ -10,15 +10,15 @@
 package org.eclipse.hawkbit.repository.event.remote.service;
 
 import lombok.Getter;
-import org.eclipse.hawkbit.repository.event.remote.RemoteTenantAwareEvent;
+import org.eclipse.hawkbit.repository.event.remote.AbstractRemoteEvent;
 
 @Getter
-public abstract class AbstractServiceRemoteEvent<T extends RemoteTenantAwareEvent> extends RemoteTenantAwareEvent {
+public abstract class AbstractServiceRemoteEvent<T extends AbstractRemoteEvent> extends AbstractRemoteEvent {
 
     private final T remoteEvent;
 
     protected AbstractServiceRemoteEvent(T remoteEvent) {
-        super(remoteEvent == null ? "_empty_tenant_" : remoteEvent.getTenant(), remoteEvent == null ? "_empty_source_" : remoteEvent.getSource());
+        super(remoteEvent == null ? "_empty_source_" : remoteEvent.getSource());
         this.remoteEvent = remoteEvent;
     }
 
