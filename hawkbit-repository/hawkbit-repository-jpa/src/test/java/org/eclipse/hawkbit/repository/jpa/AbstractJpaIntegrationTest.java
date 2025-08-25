d2076fc20e8eb9598a221e651305810f746c5f7f
vasilchev <vasil.ilchev@bosch.com>
2025-08-25 13:38:04 +0300

    Remove obsolete *ManagementSecuritiyTest classes with manual method tests in favor of new dynamic ManagementSecurityTest

--- hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/AbstractJpaIntegrationTest.java
+++ hawkbit-repository/hawkbit-repository-jpa/src/test/java/org/eclipse/hawkbit/repository/jpa/AbstractJpaIntegrationTest.java
@@ -10,28 +10,18 @@
 package org.eclipse.hawkbit.repository.jpa;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.hawkbit.repository.test.util.SecurityContextSwitch.runAs;
-import static org.eclipse.hawkbit.repository.test.util.SecurityContextSwitch.withUser;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.stream.StreamSupport;
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.eclipse.hawkbit.im.authentication.SpPermission;
 import org.eclipse.hawkbit.repository.exception.EntityNotFoundException;
-import org.eclipse.hawkbit.repository.exception.InsufficientPermissionException;
 import org.eclipse.hawkbit.repository.jpa.model.AbstractJpaBaseEntity_;
 import org.eclipse.hawkbit.repository.jpa.model.JpaAction;
 import org.eclipse.hawkbit.repository.jpa.model.JpaAction_;
@@ -55,7 +45,6 @@ import org.eclipse.hawkbit.repository.jpa.repository.TargetTagRepository;
 import org.eclipse.hawkbit.repository.jpa.repository.TargetTypeRepository;
 import org.eclipse.hawkbit.repository.jpa.repository.TenantMetaDataRepository;
 import org.eclipse.hawkbit.repository.jpa.specifications.DistributionSetSpecification;
-import org.eclipse.hawkbit.repository.jpa.specifications.TargetSpecifications;
 import org.eclipse.hawkbit.repository.model.Action;
 import org.eclipse.hawkbit.repository.model.Action.ActionStatusCreate;
 import org.eclipse.hawkbit.repository.model.DistributionSet;
@@ -222,82 +211,6 @@ public abstract class AbstractJpaIntegrationTest extends AbstractIntegrationTest
     protected void assertScheduled(final Rollout rollout, final int count) {
         final Page<JpaAction> running = actionRepository.findByRolloutIdAndStatus(PAGE, rollout.getId(), Action.Status.SCHEDULED);
         assertThat(running.getTotalElements()).as("Action count").isEqualTo(count);
-    }
-
-    /**
-     * Asserts that the given callable throws an InsufficientPermissionException.
-     * If callable succeeds without any exception or exception other than InsufficientPermissionException, it will be considered as an assert failure.
-     *
-     * @param callable the callable to call
-     */
-    protected void assertPermissions(final Callable<?> callable, List<String> requiredPermissions) {
-        assertPermissions(callable, requiredPermissions, null);
-    }
-
-    /**
-     * Asserts that the given callable throws an InsufficientPermissionException.
-     *
-     * @param callable the callable to call
-     * @param requiredPermissions required permissions for the callable
-     * @param insufficientPermissions can be null, if null, it will be resolved automatically. But in some cases (e.g. @PreAuthorized Permissions with OR, it is safer to pass directly the insufficient permissions)
-     */
-    @SneakyThrows
-    protected void assertPermissions(final Callable<?> callable, final List<String> requiredPermissions,
-            final List<String> insufficientPermissions) {
-        // if READ_PERMISSION is required and required permissions are multiple, give only READ_PERMISSION to eliminate internal read_permission check failure that would confuse the actual test
-        final List<String> resolvedInsufficientPermissions = insufficientPermissions != null
-                ? insufficientPermissions
-                : requiredPermissions.contains(SpPermission.READ_REPOSITORY) && requiredPermissions.size() > 1
-                        ? List.of(SpPermission.READ_REPOSITORY)
-                        : REPOSITORY_AND_TARGET_PERMISSIONS.stream().filter(p -> !requiredPermissions.contains(p)).toList();
-        // check if the user has the correct permissions
-        runAs(withUser("user_with_permissions", requiredPermissions.toArray(new String[0])), () -> {
-            assertPermissionWorks(callable);
-            log.info("assertPermissionWorks Passed");
-        });
-
-        // check if the user has the insufficient permissions
-        runAs(withUser("user_without_permissions", resolvedInsufficientPermissions.toArray(new String[0])), () -> {
-            assertInsufficientPermission(callable);
-            log.info("assertInsufficientPermission Passed");
-        });
-    }
-
-    /**
-     * Asserts that the given callable throws an InsufficientPermissionException.
-     * If callable succeeds without any exception or exception other than InsufficientPermissionException, it will be considered as an assert failure.
-     *
-     * @param callable the callable to call
-     */
-    private void assertInsufficientPermission(final Callable<?> callable) {
-        try {
-            callable.call();
-            throw new AssertionError(
-                    "Expected Exception 'InsufficientPermissionException' to be thrown, but request passed with no exception.");
-        } catch (Exception ex) {
-            assertThat(ex).isInstanceOf(InsufficientPermissionException.class);
-        }
-    }
-
-    /**
-     * Asserts that the given callable succeeds.
-     * </p>
-     * Note: This method will assume that EntityNotFoundException is OK, as security tests use dummy (non-existing) IDs.
-     * It matters to either callable succeeds without any exception or at most EntityNotFoundException.
-     * All other cases will be considered as an error.
-     *
-     * @param callable the callable to call
-     */
-    private void assertPermissionWorks(final Callable<?> callable) {
-        try {
-            callable.call();
-        } catch (final Throwable th) {
-            if (th instanceof EntityNotFoundException) {
-                log.info("Expected (at most) EntityNotFoundException catch: {}", th.getMessage());
-            } else {
-                throw new AssertionError("Expected no Exception (other then EntityNotFound) to be thrown, but got: " + th.getMessage(), th);
-            }
-        }
     }
 
     protected void finishAction(final Action action) {
