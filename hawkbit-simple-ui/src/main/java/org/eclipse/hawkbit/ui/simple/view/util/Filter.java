09bd905aa9d8446efe80d1a218338a9b47b708a5
Mohamed Zenadi <mohamed.zenadi@wattsense.com>
2025-09-01 10:23:05 +0200

    simple-ui: fix NPE on filters

--- hawkbit-simple-ui/src/main/java/org/eclipse/hawkbit/ui/simple/view/util/Filter.java
+++ hawkbit-simple-ui/src/main/java/org/eclipse/hawkbit/ui/simple/view/util/Filter.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
