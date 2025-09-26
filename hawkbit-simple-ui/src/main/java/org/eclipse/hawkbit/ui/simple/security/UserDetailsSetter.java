2df73ae3e067c62519ce491bb8640db6748bfee7
Florian Bezannier <florian.bezannier@hotmail.fr>
2025-09-26 17:38:22 +0200

    refacto: oauth2

---
+++ hawkbit-simple-ui/src/main/java/org/eclipse/hawkbit/ui/simple/security/UserDetailsSetter.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.hawkbit.ui.simple.security;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Collection;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+class UserDetailsSetter extends OncePerRequestFilter {
+
+    private final SecurityContextHolderStrategy securityContextHolderStrategy;
+    private final GrantedAuthoritiesService grantedAuthoritiesService;
+
+    @Override
+    protected void doFilterInternal(@NotNull final HttpServletRequest request, @NotNull final HttpServletResponse response,
+            @NotNull final FilterChain filterChain)
+            throws ServletException, IOException {
+
+        Authentication authentication = securityContextHolderStrategy.getContext().getAuthentication();
+        Authentication newAuthentication;
+
+        if (!(authentication instanceof AnonymousAuthenticationToken) && authentication.isAuthenticated()) {
+            Collection<? extends GrantedAuthority> grantedAuthorities = grantedAuthoritiesService.getGrantedAuthorities();
+
+            if (authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+                newAuthentication = new OAuth2AuthenticationToken(oAuth2AuthenticationToken.getPrincipal(), grantedAuthorities,
+                        oAuth2AuthenticationToken.getAuthorizedClientRegistrationId());
+                if (authentication.getPrincipal() instanceof OidcUser user) {
+                    // if there is no refresh token and the access token is expired then relogin is required
+                    if (user.getIdToken().getExpiresAt() != null && Instant.now().isAfter(user.getIdToken().getExpiresAt())) {
+                        throw new AccountExpiredException("Token expired");
+                    }
+                }
+            } else {
+                newAuthentication = new UsernamePasswordAuthenticationToken(authentication.getName(), authentication.getCredentials(),
+                        grantedAuthorities);
+            }
+
+            securityContextHolderStrategy.getContext().setAuthentication(newAuthentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
