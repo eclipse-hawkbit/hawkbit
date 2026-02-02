c03b788fcc76366273d65ba2d608ca8c1383a0c0
Avgustin Marinov <Avgustin.Marinov@bosch.com>
2026-02-02 14:59:00 +0200

    Add baselines for hawkbit 1.0
    Signed-off-by: Avgustin Marinov <Avgustin.Marinov@bosch.com>

--- hawkbit-repository/hawkbit-repository-jpa-flyway/src/main/resources/db/migration/POSTGRESQL/V1_12_35__sm_type_min_artifacts__POSTGRESQL.sql
+++ hawkbit-repository/hawkbit-repository-jpa-flyway/src/main/resources/db/migration/POSTGRESQL/V1_12_35__sm_type_min_artifacts__POSTGRESQL.sql
@@ -1,4 +1,4 @@
 ALTER TABLE sp_software_module_type ADD COLUMN min_artifacts integer default 0 NOT NULL;
-DROP INDEX sp_idx_distribution_set_01;
+DROP INDEX sp_idx_distribution_set_01_sp_distribution_set;
 CREATE INDEX sp_idx_distribution_set_01 ON sp_distribution_set USING BTREE (tenant, deleted);
 ALTER TABLE sp_distribution_set DROP COLUMN complete;
