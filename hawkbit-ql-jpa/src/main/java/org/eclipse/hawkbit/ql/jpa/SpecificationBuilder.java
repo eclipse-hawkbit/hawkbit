e818474e8cd6ed7e1530290a1bf9a8ccd7edcd00
Vasil Ilchev <vasil.ilchev@bosch.com>
2026-08-12 08:33:06 +0300

    RSQL map filters: index-driven joins, fix ghost joins (#3273)
    Signed-off-by: vasilchev <vasil.ilchev@bosch.com>

--- hawkbit-ql-jpa/src/main/java/org/eclipse/hawkbit/ql/jpa/SpecificationBuilder.java
+++ hawkbit-ql-jpa/src/main/java/org/eclipse/hawkbit/ql/jpa/SpecificationBuilder.java
@@ -154,10 +154,11 @@ public class SpecificationBuilder<T> {
                                 INVALID_SYNTAX,
                                 String.format("Operator %s is not supported for map fields with value null", op));
                     };
+                } else if (isNot(op)) {
+                    return compare(comparison, toMapValuePath(pathResolver.getJoinOnInner(attribute, split[1])));
                 } else {
-                    // map entry with key not null (exist) - correlated EXISTS semi-join per predicate;
-                    // avoids the LEFT JOIN + DISTINCT row explosion on large attribute tables
-                    return existsMapEntry(comparison, attribute, split[1]);
+                    final MapJoin<?, ?, ?> mapPath = (MapJoin<?, ?, ?>) pathResolver.getPath(attribute);
+                    return cb.and(equal(mapPath.key(), split[1]), compare(comparison, toMapValuePath(mapPath)));
                 }
             } else if (attribute instanceof SetAttribute<?, ?> setAttribute) {
                 if (split.length < 2 || ObjectUtils.isEmpty(split[1])) {
@@ -213,14 +214,6 @@ public class SpecificationBuilder<T> {
         private static Path<String> toMapValuePath(final Path<?> mapJoin) {
             final Path<?> valuePath = ((MapJoin<?, ?, ?>) mapJoin).value();
             return valuePath.getJavaType() == String.class ? (Path<String>) valuePath : valuePath.get("value");
-        }
-
-        // correlated EXISTS semi-join for a map entry with a non-null value filter (key/value in the subquery where)
-        private Predicate existsMapEntry(final Comparison comparison, final Attribute<? super T, ?> attribute, final String mapKey) {
-            final Subquery<Integer> subquery = query.subquery(Integer.class);
-            final MapJoin<?, ?, ?> mapJoin = (MapJoin<?, ?, ?>) subquery.correlate(root).join(attribute.getName(), JoinType.INNER);
-            return cb.exists(subquery.select(cb.literal(1))
-                    .where(cb.and(equal(mapJoin.key(), mapKey), compare(comparison, toMapValuePath(mapJoin)))));
         }
 
         private Predicate compare(final Comparison comparison, final Path<?> fieldPath) {
@@ -469,6 +462,10 @@ public class SpecificationBuilder<T> {
                 return getCollectionPathResolver(attribute.getName()).getJoinOn(value);
             }
 
+            private MapJoin<?, ?, ?> getJoinOnInner(final Attribute<?, ?> attribute, final Object value) {
+                return getCollectionPathResolver(attribute.getName()).getJoinOnInner(value);
+            }
+
             private Map<String, Integer> getState() {
                 return attributeToPathResolver.entrySet().stream()
                         .collect(Collectors.toMap(Map.Entry::getKey, resolver -> resolver.getValue().getPos()));
@@ -491,6 +488,7 @@ public class SpecificationBuilder<T> {
                 @Setter
                 private int pos;
                 private final Map<Object, MapJoin<?, ?, ?>> joinOnCache = new HashMap<>();
+                private final Map<Object, MapJoin<?, ?, ?>> joinOnInnerCache = new HashMap<>();
 
                 private CollectionPathResolver(final String attributeName) {
                     this.attributeName = attributeName;
@@ -510,6 +508,14 @@ public class SpecificationBuilder<T> {
                 private MapJoin<?, ?, ?> getJoinOn(final Object value) {
                     return joinOnCache.computeIfAbsent(value, k -> {
                         final MapJoin<?, ?, ?> mapPath = (MapJoin<?, ?, ?>) root.join(attributeName, JoinType.LEFT);
+                        mapPath.on(equal(mapPath.key(), k));
+                        return mapPath;
+                    });
+                }
+
+                private MapJoin<?, ?, ?> getJoinOnInner(final Object value) {
+                    return joinOnInnerCache.computeIfAbsent(value, k -> {
+                        final MapJoin<?, ?, ?> mapPath = (MapJoin<?, ?, ?>) root.join(attributeName, JoinType.INNER);
                         mapPath.on(equal(mapPath.key(), k));
                         return mapPath;
                     });
