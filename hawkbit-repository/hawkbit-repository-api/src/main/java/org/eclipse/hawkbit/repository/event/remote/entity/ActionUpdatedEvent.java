3d4d93bb3236e9a18bc3ab6f8d929cd05ba299a2
strailov <Stanislav.Trailov@bosch.io>
2026-09-09 16:54:40 +0300

    Extend Action Updated Remote Event
    Signed-off-by: strailov <Stanislav.Trailov@bosch.io>

--- hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/event/remote/entity/ActionUpdatedEvent.java
+++ hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/event/remote/entity/ActionUpdatedEvent.java
@@ -12,21 +12,32 @@ package org.eclipse.hawkbit.repository.event.remote.entity;
 import java.io.Serial;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.eclipse.hawkbit.repository.event.entity.EntityUpdatedEvent;
 import org.eclipse.hawkbit.repository.model.Action;
 
 /**
  * Defines the remote event of updated a {@link Action}.
  */
-@NoArgsConstructor // for serialization libs like jackson
+@NoArgsConstructor(force = true) // for serialization libs like jackson
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class ActionUpdatedEvent extends AbstractActionEvent implements EntityUpdatedEvent {
 
     @Serial
-    private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 3L;
+
+    private final Action.Status status;
+    private final boolean downloadOnly;
 
     @JsonIgnore
     public ActionUpdatedEvent(final Action action, final Long targetId, final Long rolloutId, final Long rolloutGroupId) {
         super(action, targetId, rolloutId, rolloutGroupId);
+        this.status = action.getStatus();
+        this.downloadOnly = action.isDownloadOnly();
     }
 }
